766ba1dc48fb7e39fa2fd712aa1399535cad9725
lusate <95400441+lusate@users.noreply.github.com>
2022-07-06 01:26:03 +0900

    Create 특정 문자 뒤집기.java

---
+++ 특정 문자 뒤집기.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Main {
+    public String Solution(String s){
+        String answer = "";
+        //s 라는 문자열을 char로 바꾼다
+        char[] str = s.toCharArray();
+
+        int left = 0;
+        int right = s.length()-1;
+
+        while(left < right){
+            //str[left]가 알파벳이 아니면
+            if(!Character.isAlphabetic(str[left])){
+                left++;
+            }
+            //str[right]가 알파벳이 아니면
+            else if(!Character.isAlphabetic(str[right])){
+                right--;
+            }
+            //둘 다 알파벳이라면 서로 위치를 바꾸고 다음 셋으로
+            else{
+                char tmp = str[left];
+                str[left] = str[right];
+                str[right] = tmp;
+                left++;
+                right--;
+            }
+        }
+        //answer = str;로 그냥 str 부를 수 없다. (str은 char이기 때문)
+        answer = String.valueOf(str);
+        return answer;
+    }
+    public static void main(String[] args){
+        Main T = new Main();
+        System.out.println(T.Solution("a#b!GE*T@S"));
+    }
+}
