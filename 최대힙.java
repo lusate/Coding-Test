d8e2ee13e864dc4dea09639d16577c8a14935e0e
lusate <95400441+lusate@users.noreply.github.com>
2022-07-15 16:04:32 +0900

    Create 최대힙.java

---
+++ 최대힙.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Main {
+	public ArrayList<Integer> solution(int[] nums){
+		ArrayList<Integer> answer = new ArrayList<>();
+		PriorityQueue<Integer> pQ = new PriorityQueue<>(Collections.reverseOrder());
+		//이렇게 하면 최대힙이 됨
+		
+		for(int x : nums){
+			if(x == 0){//큐에서 하나 꺼냄
+				if(pQ.isEmpty()) answer.add(-1); //큐가 비어있으면 -1
+				else answer.add(pQ.poll());
+			}
+			else{ //0이 아니면
+				pQ.offer(x);
+			}
+		}
+
+		return answer;
+	}
+	public static void main(String[] args){
+		Main T = new Main();
+		int[] arr = new int[]{5, 3, 6, 0, 5, 0, 2, 4, 0};
+		System.out.println(T.solution(arr));
+	} 
+}
