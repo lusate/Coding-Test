e8037c7e242319bff56ed5a6ad84c993c36ce7f9
lusate <95400441+lusate@users.noreply.github.com>
2022-07-19 16:03:55 +0900

    Create 결혼식.java

---
+++ 결혼식.java
@@ -0,0 +1,11 @@
+//결혼식
+/*import java.util.*;
+class Main{
+	public int solution(int[][] times){
+		
+	}
+	public static void main(String[] args){
+		Main T = new Main();
+		System.out.println(T.solution(new int[][]{{14, 18}, {12, 15}, {15, 20}, {20, 30}, {5, 14}}));
+	}
+}*/
