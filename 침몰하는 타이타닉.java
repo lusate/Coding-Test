506a59654b9ba328e189c2aaad45118a5382f016
lusate <95400441+lusate@users.noreply.github.com>
2022-07-19 21:42:38 +0900

    Create 침몰하는 타이타닉.java

---
+++ 침몰하는 타이타닉.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Main{
+	public int solution(int[] nums, int m){
+		int answer=0;  //보트 개수
+		int left=0;
+		int right=nums.length;
+
+		Arrays.sort(nums);
+		//양쪽에서 2개를 비교
+		while(left <= right){
+			if(nums[left] + nums[right] <= m){
+				answer++; //보트 수 추가
+				left++;
+				right--;
+			}
+			else{//m보다 커져도 보트 수 하나가 필요
+				answer++;
+				right--;
+			}
+		}
+		
+		return answer;
+	}
+	public static void main(String[] args){
+		Main T = new Main();
+		System.out.println(T.solution(new int[]{90, 50, 70, 100, 60}, 140));
+		System.out.println(T.solution(new int[]{10, 20, 30, 40, 50, 60, 70, 80, 90}, 100));
+	}
+}
