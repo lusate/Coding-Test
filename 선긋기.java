04e921f862ab42cbb28de281e2f98a26dc541457
lusate <95400441+lusate@users.noreply.github.com>
2022-07-19 16:03:03 +0900

    Create 선긋기.java

---
+++ 선긋기.java
@@ -0,0 +1,14 @@
+//선긋기
+import java.util.*;
+class Main {
+	public int solution(int[][] nums){
+		
+		return answer;
+	}
+
+	public static void main(String[] args){
+		Main T = new Main();
+		System.out.println(T.solution(new int[][]{{1, 3}, {2, 5}, {7, 10}}));
+		System.out.println(T.solution(new int[][]{{5, 6}, {1, 3}, {7, 8}, {9, 10}}));
+	}
+}
