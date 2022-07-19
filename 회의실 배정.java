e0f0241f81e86b3f93a21bdaabf6401e9f72fadc
lusate <95400441+lusate@users.noreply.github.com>
2022-07-19 16:03:36 +0900

    Create 회의실 배정.java

---
+++ 회의실 배정.java
@@ -0,0 +1,12 @@
+
+//회의실 배정
+import java.util.*;
+class Main{
+	public int solution(int[][] times){
+	
+    }
+	public static void main(String[] args){
+		Main T = new Main();
+		System.out.println(T.solution(new int[][]{{1, 4}, {2, 3}, {3, 5}, {4, 6}, {5, 7}}));
+	}
+}
