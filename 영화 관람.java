5b6af8c3a194a0dfe1ff9fab1d941e52ed0a726e
lusate <95400441+lusate@users.noreply.github.com>
2022-07-23 20:59:30 +0900

    Update 영화 관람.java

--- 영화 관람.java
+++ 영화 관람.java
@@ -9,7 +9,7 @@ class Main{
 		for(int i=n-1; i>=0; i--){
 			//매우 중요 
 			//스택에 최초로 시청을 방해하는 학생의 키가 아닌 번호를 반환해야 하므로 인덱스 번호를 스택에 넣고 뺀다.
-			while(!stack.empty() && nums[i] > nums[stack.peek()]){ //방해자 발견
+			while(!stack.empty() && nums[i] > nums[stack.peek()]){ //나보다 큰 방해자(nums[i])를 발견
 				answer[stack.peek()] = i+1; //번호를 찾아줌 (인덱스 + 1)
 				stack.pop();
 			}
