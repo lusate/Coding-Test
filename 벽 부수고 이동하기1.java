d99790f009b1ddb43f8294bbff45ebf862ed9126
lusate <95400441+lusate@users.noreply.github.com>
2022-07-29 22:27:33 +0900

    Update 벽 부수고 이동하기1.java

--- 벽 부수고 이동하기1.java
+++ 벽 부수고 이동하기1.java
@@ -22,38 +22,37 @@ class Main {
 		int[] dx = {-1, 0, 1, 0};
 		int[] dy = {0, 1, 0, -1};
 
-		Queue<Point> q = new LinkedList<>();
-        q.add(new Point(x, y, 1, 0));
-        visited[x][y][0] = true; //0은 벽을 부시지 않고 방문한 노드의 방문 여부
+		Queue<Point> Q = new LinkedList<>();
+        Q.add(new Point(0, 0, 1, 0));
+		visited[x][y][0] = true; //0은 벽을 부시지 않고 방문한 노드의 방문 여부
         visited[x][y][1] = true; //1은 벽을 부시면서 방문한 노드의 방문 여부
  
-        while (!q.isEmpty()) {
-          Point cur = q.poll();
-          //cur.x , cur.y는 현재 좌표
-          if (cur.x == n - 1 && cur.y == m - 1) return cur.count;
-          // 문제에서  (1, 1)과 (N, M)은 항상 0이라고 가정하자. 이동거리를 리턴
- 
-          for (int i = 0; i < 4; i++) {
-            int nx = cur.x + dx[i];
-            int ny = cur.y + dy[i];
- 
-            if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
-              if(board[nx][ny] == 0) { //벽이 아닐 때
-                //현재까지 온 방법
-                if (visited[nx][ny][cur.wall] == false) { 
-                //(벽을 부쉈는지 아닌지)으로 방문한 적이 없다면 방문한다.
-                  q.add(new Point(nx, ny, cur.count + 1, cur.wall));
-                  visited[nx][ny][cur.wall] = true;
-                  }
-              }    
-              else if (board[nx][ny] == 1) { //벽일때
-                if (cur.wall == 0 && visited[nx][ny][1] == false) { //현재까지 벽을 부순적이 없고, 벽을 부숴서 방문한 적이 없다면 방문한다.
-                  q.add(new Point(nx, ny, cur.count + 1, 1));
-                  visited[nx][ny][1] = true;
-                }
-              }
-            }
-          }
+        while (!Q.isEmpty()) {
+			Point tmp = Q.poll();
+			//cur.x , cur.y는 현재 좌표
+			if (tmp.x == n - 1 && tmp.y == m - 1) return tmp.count;
+			// 문제에서  (1, 1)과 (N, M)은 항상 0이라고 가정하자. 이동거리를 리턴
+	
+			for (int i = 0; i < 4; i++) {
+				int nx = tmp.x + dx[i];
+				int ny = tmp.y + dy[i];
+	
+				if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
+					if(board[nx][ny] == 0) { //벽이 없을 때
+						if (visited[nx][ny][tmp.wall] == false) { 
+							//(벽을 부쉈는지 아닌지)으로 방문한 적이 없다면 방문한다.
+							visited[nx][ny][tmp.wall] = true;
+							Q.offer(new Point(nx, ny, tmp.count + 1, tmp.wall));
+						}
+					}    
+					else if (board[nx][ny] == 1) { //벽일때
+						if (tmp.wall == 0 && visited[nx][ny][1] == false) { //현재까지 벽을 부순적이 없고, 벽을 부숴서 방문한 적이 없다면 방문한다.
+							visited[nx][ny][1] = true;
+							Q.offer(new Point(nx, ny, tmp.count + 1, 1));
+						}
+					}
+				}
+			}
         }
  
         return -1;
