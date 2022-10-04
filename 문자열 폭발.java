3a2c573f587356845de181347a73ddfd904b719f
lusate <95400441+lusate@users.noreply.github.com>
2022-10-04 20:16:01 +0900

    Update 문자열 폭발.java

--- 문자열 폭발.java
+++ 문자열 폭발.java
@@ -6,22 +6,21 @@ class Main{
 	static Stack<Character> stack = new Stack<>();
 
 	public String solution(String s, String bomb) {
-		String answer="";
-		
 		for(int i=0; i<s.length(); i++){
 			//스택에 s를 한 번에 다 넣는 것이 아님.
 			stack.push(s.charAt(i));
 
 			//스택에 있는 문자열의 길이가 bomb보다 길어야 함.
 			if(stack.size() >= bomb.length()){
-				boolean contain = true;
+				boolean contain = true; //bomb에 있는 문자가 s에 포함되면 true
 				for(int j=0; j<bomb.length(); j++){
 					if(stack.get(stack.size() - bomb.length() + j) != bomb.charAt(j)){
+						//포함되어 있지 않으면 false
 						contain = false;
 						break;
 					}
 				}
-				if(contain){
+				if(contain){ //포함된 경우 pop
 					for(int j=0; j<bomb.length(); j++){
 						stack.pop();
 					}
@@ -29,23 +28,16 @@ class Main{
 			}
 			//System.out.println(stack);
 		}
-		
-		/*if(stack.empty()){
-			answer = "FRULA";
-		}
-		else{
-			for(Character c : stack){
-				answer += c;
-			}
-		}*/
 
+		
+		// 쪼개져있는 문자들을 문자열로 만들어주기
 		StringBuilder sb = new StringBuilder();
 		for(Character c : stack) {
 			sb.append(c);
 		}
 		
 		// 문자열의 길이가 0이라면 FRULA 출력, 아니라면 문자열 출력
-		return sb.length() == 0? "FRULA" : sb.toString();
+		return sb.length() == 0 ? "FRULA" : sb.toString();
 		
 	}
 	public static void main(String[] args) throws Exception {
@@ -56,8 +48,6 @@ class Main{
 		bomb = br.readLine();
 
 		System.out.println(T.solution(s, bomb));
-		//System.out.println(T.solution("mirkovC4nivCC44", "C4"));
-		//System.out.println(T.solution("12ab112ab2ab", "12ab"));
 	}
 }
 
