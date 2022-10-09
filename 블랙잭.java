a23dff29813d08d62df20f7e4f2238d3a6ee231f
lusate <95400441+lusate@users.noreply.github.com>
2022-10-09 22:47:15 +0900

    Update 블랙잭.java

--- 블랙잭.java
+++ 블랙잭.java
@@ -1,6 +1,42 @@
 //백준
+import java.util.*;
+public class Main {
+	public int solution(int[] choice, int n, int m) {
+        int answer=0; //3장의 카드를 더한 변수
+		int sum=0; //이전 sum의 값을 저장하기 위한 변수
+		
 
+		for(int i=0; i<n-2; i++){
+			for(int j=i+1; j<n-1; j++){
+				for(int k=j+1; k<n; k++){
+					sum = choice[i] + choice[j] + choice[k];
+					if(sum == m){
+						return sum;
+					}
+					else if(sum < m && answer < sum){
+						//m보다 작고 이전 sum 값보다 작아야함.
+						answer = sum;
+					}
+				}
+			}
+		}
+        return answer;
+    }
+	public static void main(String[] args){
+		Main T = new Main();
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int[] choice = new int[n];
+		for(int i=0; i<n; i++){
+			choice[i] = sc.nextInt();
+		}
 
+		System.out.println(T.solution(choice, n, m));
+	}
+}
+
+----------------------------------------------------------------------------------------------
 
 //프로그래머스
 import java.util.*;
