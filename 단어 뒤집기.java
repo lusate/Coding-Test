bb676f88adca63d3a27e7aadcb333a7f28068032
lusate <95400441+lusate@users.noreply.github.com>
2022-10-20 18:50:30 +0900

    Update 단어 뒤집기.java

--- 단어 뒤집기.java
+++ 단어 뒤집기.java
@@ -26,3 +26,56 @@ public class Main{
 		}
 	}
 }
+
+
+--------------------------------------------------------------------------------------
+
+import java.util.*;
+public class Main{
+	public ArrayList<String> solution(int n, String[] str){
+		ArrayList<String> answer = new ArrayList<>();
+		
+		for(String x : str){
+			char[] s = x.toCharArray();
+			int left = 0;
+			int right = x.length()-1;
+
+			while(left < right){
+				char tmp = s[left];
+				s[left] = s[right];
+				s[right] = tmp;
+				left++;
+				right--;
+			}
+
+			String tmp = String.valueOf(s);
+			answer.add(tmp);
+		}
+		return answer;
+	}
+
+	public static void main(String[] args){
+		Main T = new Main();
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		String[] str = new String[n];
+
+		for(int i=0; i<n; i++){
+			str[i] = sc.next();
+		}
+
+		for(String x : T.solution(n, str)){
+			System.out.println(x);
+		}
+	}
+}
+
+
+//입력
+2
+good
+doig
+
+//출력
+//doog
+//giod
