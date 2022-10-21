d0cb4ffc88ac814022b61f3b311fc5b1334c960f
lusate <95400441+lusate@users.noreply.github.com>
2022-10-21 17:18:06 +0900

    Update 최대 상금(싸피).java

--- 최대 상금(싸피).java
+++ 최대 상금(싸피).java
@@ -1,61 +1,65 @@
 import java.util.*;
 import java.io.*;
 
-class Solution{
-	static int[] arr; //점수판
-	static int max = 0;
-	public static void dfs(int k, int cnt, int chance){ //k는 시작 인덱스
-		if(chance == cnt){
-			//기회 모두 씀
-			StringBuilder sb = new StringBuilder(); //문자열을 변형 시키기 위함.
-			for(int i : arr){
-				sb.append(i); //arr 배열에 있는 문자열을 sb에 넣음.
-			}
+class Solution {
+    // 전역 배열 (점수판)
+    static int[] numbers;
+    // 최대 값
+    static int max = 0;
+	static String str;
+    // dfs 함수 (인덱스, 변동 제한 수)
+    public static void dfs(int start, int chance) {
+        if(chance == 0){ //바꾸는 기회가 더 이상 없으면
+			str = ""; //num 숫자를 넣을 문자.
 
-			//sb에 있는 문자열을 숫자로 변환해서 max 비교하면서 초기화해줌.
-			max = Math.max(max, Integer.parseInt(sb.toString()));
+			//int[] numbers 에 v를 문자열로 바꿔서 각각에 더해줌.
+			Arrays.stream(numbers).forEach(v -> str += String.valueOf(v));
+			int price = Integer.parseInt(str);
+			if(price > max) max = price;
 			return;
 		}
+		
+		for(int i=start; i<numbers.length-1; i++){
+			for(int j=i+1; j<numbers.length; j++){
+				int temp = numbers[i];
+				numbers[i] = numbers[j];
+				numbers[j] = temp;
 
-		for(int i=k; i<arr.length; i++){
-			for(int j=i+1; j<arr.length; j++){
-				if(arr[i] <= arr[j]){
-					int temp = arr[i];
-					arr[i] = arr[j];
-					arr[j] = temp;
+				dfs(i, chance - 1);
 
-					dfs(i, cnt+1, chance);
-					temp = arr[i];
-					arr[i] = arr[j];
-					arr[j] = temp;
-				}
+				numbers[j] = numbers[i];
+				numbers[i] = temp;
 			}
 		}
-	}
-	public static void main(String args[]) throws Exception{
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
+    }
+    public static void main(String args[]) throws Exception{
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for(int test_case = 1 ; test_case <= T ; test_case++) {
+            // max는 항상 초기화 해줘야함
+            max = 0;
+            // 번호판 입력
+            int num = sc.nextInt();
+            // 제한 횟수 입력
+            int chance = sc.nextInt();
+            
+			//정수형 num을 문자열로 바꾸고 char[] 에 저장.
+			char[] ch = Integer.toString(num).toCharArray();
 
-		for(int test_case = 1; test_case <= T; test_case++){
-			//최댓값 초기화
-			max = 0;
+			numbers = new int[ch.length];
 
-			//번호 입력
-			int num = sc.nextInt();
+			for(int i=0; i<ch.length; i++){
+				numbers[i] = ch[i] - '0';
+			}//문자를 다시 숫자로
 
-			//바꿀 기회 입력
-			int chance = sc.nextInt();
-			
-			//int -> int[] 바꾸는 방법
-			arr = Integer.toString(num).chars().map(c -> c - '0').toArray();
-			//num을 String 바꾸고 다시 chars 로 변환
-			//그 다음 문자 - '0' => 즉 (문자 - 48) 하고 toArray()로 배열로 만들어줌.
+			//123 으로 length가 3인데 chance 가 5이라 하면 chance = 3으로
+			//즉 number 길이가 chance보다 크면 안됨.
+			if(numbers.length < chance) chance = numbers.length;
+			dfs(0, chance);
 
-			dfs(0, 0, chance);
 			System.out.println("#" + test_case + " " + max);
-		}
-		return;
-	}
+        }
+    }
 }
 
 
@@ -75,5 +79,14 @@ class Solution{
 
 
 /* 출력
-#1 ㅇㄹ
+#1 321
+#2 7732
+#3 857147
+#4 87664
+#5 88832
+#6 777770
+#7 966354
+#8 954311
+#9 332211
+#10 987645
 */
