1e7f9481a0fce40e3503db07a59d06ad1cbadb01
lusate <95400441+lusate@users.noreply.github.com>
2022-10-27 14:41:59 +0900

    Update 숨바꼭질 3.java

--- 숨바꼭질 3.java
+++ 숨바꼭질 3.java
@@ -1,43 +1,65 @@
 import java.util.*;
-import java.io.*;
 class Point{
 	int x;
 	int time;
-	public Point(int x, int time){
+	Point(int x, int time){
 		this.x = x;
 		this.time = time;
 	}
 }
-public class Main {
-	public int solution(int n, int k) {
-		Queue<Point> Q = new LinkedList<>();
-		int[] ch = new int[10001];
-		int min = Integer.MAX_VALUE;
-		Q.offer(new Point(n, 0));
-		while(!Q.isEmpty()){
-			Point point = Q.poll();
-			ch[point.x] = 1;
-			if(point.x == k) min = Math.min(min, point.time);
+class Main{
+	static int L;
+	static int cnt;
+	static int[] ch;
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int k = sc.nextInt();
 
-			if(point.x * 2 <= 10000 && ch[point.x * 2] == 0){
-				Q.offer(new Point(point.x * 2, point.time));
+		L = Integer.MAX_VALUE;
+		cnt = 0;
+
+		Queue<Point> Q = new LinkedList<>();
+		ch = new int[100001];
+		ch[n] = 1;
+		Q.offer(new Point(n, 1));
+		while(!Q.isEmpty()){
+			Point tmp = Q.poll();
+
+			//뒤로 한 칸
+			if(tmp.x - 1 >= 0 && tmp.x - 1 < 100001){
+				if(ch[tmp.x - 1] == 0 || ch[tmp.x - 1] > tmp.time+1){
+					ch[tmp.x - 1] = tmp.time + 1;
+					Q.offer(new Point(tmp.x-1, tmp.time+1));
+				}
 			}
-			if(point.x + 1 <= 10000 && ch[point.x + 1] == 0){
-				Q.offer(new Point(point.x + 1, point.time + 1));
+
+			//앞으로 한 칸
+			if(tmp.x + 1 >= 0 && tmp.x + 1 < 100001){
+				if(ch[tmp.x + 1] == 0 || ch[tmp.x + 1] > tmp.time+1){
+					ch[tmp.x + 1] = tmp.time + 1;
+					Q.offer(new Point(tmp.x + 1, tmp.time + 1));
+				}
 			}
-			if(point.x - 1 >= 0 && ch[point.x - 1] == 0){
-				Q.offer(new Point(point.x - 1, point.time + 1));
+
+			//순간이동
+			if(tmp.x*2 >= 0 && tmp.x*2 < 100001){
+				if(ch[tmp.x*2] == 0 || ch[tmp.x*2] > tmp.time){
+					ch[tmp.x*2] = tmp.time;
+					Q.offer(new Point(tmp.x*2, tmp.time));
+				}
 			}
 		}
 
-    	return min;
-    }
-	public static void main(String[] args){
-		Main T = new Main();
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt(); //수빈
-		int k = sc.nextInt(); //동생
-		
-		System.out.println(T.solution(n, k));
+		System.out.println(ch[k] - 1);
 	}
 }
+
+
+/* 입력
+5 17
+*/
+
+/* 출력
+2
+*/
