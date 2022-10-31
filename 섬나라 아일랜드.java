a16a6b8205f234ea580f8eb328c2547e984a9924
lusate <95400441+lusate@users.noreply.github.com>
2022-10-31 18:05:03 +0900

    Update 섬나라 아일랜드.java

--- 섬나라 아일랜드.java
+++ 섬나라 아일랜드.java
@@ -33,6 +33,64 @@ class Main { //송아지처럼 목적지를 찾아서 가는 것이 아니고 �
 		System.out.println(T.solution(new int[][]{{1, 1, 0, 0, 0, 1, 0}, {0, 1, 1, 0, 1, 1, 0}, {0, 1, 0, 0, 0, 0, 0}, {0, 0, 0, 1, 0, 1, 1}, {1, 1, 0, 1, 1, 0, 0}, {1, 0, 0, 0, 1, 0, 0}, {1, 0, 1, 0, 1, 0, 0}}));
 	}
 }
+-------------------------------------------------------------------------------------------------------
+//백준
+import java.util.*;
+class Main {
+	static int[] dx={-1, -1, 0, 1, 1, 1, 0, -1};
+	static int[] dy={0, 1, 1, 1, 0, -1, -1, -1};
+	static int answer = 0, n;
+	
+	public void DFS(int x, int y, int[][] board){
+		for(int i=0; i<8; i++){
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			if(nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] == 1){
+				board[nx][ny] = 0;
+				DFS(nx, ny, board);
+			}
+		}
+	}
 
+	public void solution(int[][] board){
+		for(int i=0; i<n; i++){
+			for(int j=0; j<n; j++){
+				if(board[i][j] == 1){ //섬을 만나는 경우
+					answer++;
+					board[i][j] = 0; //출발점을 0으로
+					DFS(i, j, board); //0이 처음 발견되는 좌표.
+				}
+			}
+		}
+	}
 
-//5
+	public static void main(String[] args){
+		Main T = new Main();
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		int[][] arr = new int[n][n];
+		for(int i=0; i<n; i++){
+			for(int j=0; j<n; j++){
+				arr[i][j] = sc.nextInt();
+			}
+		}
+		T.solution(arr);
+		System.out.println(answer);
+	}
+}
+	
+
+/* 입력
+7
+1 1 0 0 0 1 0
+0 1 1 0 1 1 0
+0 1 0 0 0 0 0
+0 0 0 1 0 1 1
+1 1 0 1 1 0 0
+1 0 0 0 1 0 0
+1 0 1 0 1 0 0
+*/
+
+/* 출력
+5
+*/
