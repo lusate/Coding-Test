167d3d3d18bda97078d1d769e6716260252ce0b5
lusate <95400441+lusate@users.noreply.github.com>
2022-11-12 16:00:03 +0900

    Update 플로이드 와샬.java

--- 플로이드 와샬.java
+++ 플로이드 와샬.java
@@ -40,4 +40,80 @@ class Main {
 	}
 }
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//백준 플로이드
+import java.util.*;
+public class Main {
+	static int INF = 1000000;
+	public int solution(int n, int m, int[][] dy) {
+		for(int k=1; k<=n; k++) {
+			for(int i=1; i<=n; i++) {
+				for(int j=1; j<=n; j++) {
+					if(dy[i][j] > dy[i][k] + dy[k][j]) {
+						dy[i][j] = dy[i][k] + dy[k][j];
+					}
+				}
+			}
+		}
+		
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<=n; j++) {
+				if(dy[i][j] == INF) dy[i][j] = 0;
+				else System.out.print(dy[i][j] + " ");
+			}
+			System.out.println();
+		}
+		return 0;
+	}
+	
+	public static void main(String[] args) throws Exception {
+		Main T = new Main();
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt(); //도시 개수
+		int m = sc.nextInt(); //버스 개수
+		int[][] dy = new int[n+1][n+1];
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<=n; j++) {
+				if(i == j) dy[i][j] = 0;
+				else dy[i][j] = INF;
+			}
+		}
+		
+		for(int i=1; i<=m; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			int c = sc.nextInt(); //비용
+			
+			dy[a][b] = Math.min(dy[a][b], c);
+		}
+		T.solution(n,m,dy);
+	}
+}
 
+/*입력 
+5
+14
+1 2 2
+1 3 3
+1 4 1
+1 5 10
+2 4 2
+3 4 1
+3 5 1
+4 5 3
+3 5 10
+3 1 8
+1 4 2
+5 1 7
+3 4 2
+5 2 4
+*/
+
+/* 출력
+0 2 3 1 4 
+12 0 15 2 5 
+8 5 0 1 1 
+10 7 13 0 3 
+7 4 10 6 0
+*/
