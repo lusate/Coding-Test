42dea8a346fd471c209f7d330ff36a60c9895706
lusate <95400441+lusate@users.noreply.github.com>
2022-11-14 18:54:02 +0900

    Update 회의실 배정.java

--- 회의실 배정.java
+++ 회의실 배정.java
@@ -1,4 +1,50 @@
 import java.util.*;
+class Time implements Comparable<Time>{
+	public int s, e;
+	Time(int s, int e){
+		this.s=s;
+		this.e=e;
+	}
+	@Override
+	public int compareTo(Time ob){
+		if(this.e == ob.e) return this.s - ob.s; //같으면 시작점을 가지고 오름차순
+		else return this.e - ob.e; //끝점을 가지고 오름차순
+	}
+}
+
+class Main {
+	public int solution(int n, ArrayList<Time> arr){
+		int answer=0;
+		Collections.sort(arr);
+		int et = 0;
+		for(Time ob : arr){
+			if(et <= ob.s){
+				answer++;
+				et = ob.e;
+			}
+		}
+		System.out.println(answer);
+		return answer;
+	}
+	public static void main(String[] args)  {
+		Main T = new Main();
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		ArrayList<Time> arr = new ArrayList<>();
+		
+		for(int i=0; i<n; i++){
+			int x = sc.nextInt();
+			int y = sc.nextInt();
+			arr.add(new Time(x, y));
+		}
+		T.solution(n, arr);
+	}
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+import java.util.*;
 class Main{
 	public int solution(int[][] times){
 		int answer=0;
