7744b9ead9d958fda4fc3e9437474527cb4054ab
lusate <95400441+lusate@users.noreply.github.com>
2022-11-18 20:32:21 +0900

    Update 최대힙.java

--- 최대힙.java
+++ 최대힙.java
@@ -23,3 +23,39 @@ class Main {
 		System.out.println(T.solution(arr));
 	} 
 }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+import java.util.*;
+class Main{
+	//ps는 문제 점수, pt는 문제 타임
+	public ArrayList<Integer> solution(int[] nums){
+		ArrayList<Integer> answer = new ArrayList<>();
+		PriorityQueue<Integer> pQ = new PriorityQueue<>();
+
+		for(int x : nums){
+			if(x == 0){
+				if(pQ.isEmpty()) answer.add(-1);
+				else answer.add(pQ.poll());
+			}
+			else{
+				pQ.offer(x);
+			}
+		}
+
+		System.out.println(answer);
+		return answer;
+	}
+	public static void main(String args[]) throws Exception{
+		Main T = new Main();
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int[] nums = new int[n];
+		for(int i=0; i<n; i++){
+			nums[i] = sc.nextInt();
+		}
+		
+		T.solution(nums);
+	}
+}
