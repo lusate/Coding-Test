9d96f53e57999f3c93df9d9db0f003a13555aa25
lusate <95400441+lusate@users.noreply.github.com>
2022-11-18 20:22:32 +0900

    Create 조합수(메모이제이션).java

---
+++ 조합수(메모이제이션).java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Main {
+	public int dfs(int n, int r){
+		int[][] dy = new int[n+1][r+1];
+		if(dy[n][r] > 0) return dy[n][r];
+		if(n==r||r==0) return 1;
+		else return dfs(n-1, r-1) + dfs(n-1, r);
+	}
+	public static void main(String[] args){
+		Main T = new Main();
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int r = sc.nextInt();
+		//nCr
+
+		T.dfs(n, r);
+		System.out.println(T.dfs(n, r));
+	} 
+}
+
+
+/* 입력
+5 3
+*/
+
+/* 출력
+10
+*/
