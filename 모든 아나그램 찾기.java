7903ab541c2f61097fe10f0398447c6621ae154e
lusate <95400441+lusate@users.noreply.github.com>
2022-11-19 21:59:49 +0900

    Update 모든 아나그램 찾기.java

--- 모든 아나그램 찾기.java
+++ 모든 아나그램 찾기.java
@@ -1,36 +1,30 @@
-public class Main {
-	public int solution(String a, String b) {
+import java.util.*;
+class Main {	
+	public int solution(String a, String b){
 		int answer=0;
-		HashMap<Character, Integer> am = new HashMap<>();
-		HashMap<Character, Integer> bm = new HashMap<>();
-		for(char x : b.toCharArray()) {
-			bm.put(x,  bm.getOrDefault(x, 0) + 1);
-		}
+		HashMap<Character, Integer> am=new HashMap<>();
+		HashMap<Character, Integer> bm=new HashMap<>();
+		for(char x : b.toCharArray()) bm.put(x, bm.getOrDefault(x, 0)+1);
 		int L=b.length()-1;
-		
-		for(int i=0; i<L; i++) {
-			am.put(a.charAt(i), am.getOrDefault(a.charAt(i), 0) + 1);
-		}
-		
-		int left=0;
-		for(int right = L; right < a.length(); right++) {
-			am.put(a.charAt(right), am.getOrDefault(a.charAt(right), 0) + 1);
+		for(int i=0; i<L; i++) am.put(a.charAt(i), am.getOrDefault(a.charAt(i), 0)+1);
+		int lt=0;
+		for(int rt=L; rt<a.length(); rt++){
+			am.put(a.charAt(rt), am.getOrDefault(a.charAt(rt), 0)+1);
 			if(am.equals(bm)) answer++;
-			am.put(a.charAt(left), am.get(a.charAt(left)) - 1);
-			if(am.get(a.charAt(left)) == 0) am.remove(a.charAt(left));
-			left++;
+			am.put(a.charAt(lt), am.get(a.charAt(lt))-1);
+			if(am.get(a.charAt(lt))==0) am.remove(a.charAt(lt));
+			lt++;
 		}
-		
 		return answer;
 	}
-	
-	public static void main(String[] args) throws Exception {
-		Main T = new Main();
-		Scanner sc = new Scanner(System.in);
 		
-		String a = sc.next();
-		String b = sc.next();
-		System.out.println(T.solution(a, b));
+
+	public static void main(String[] args){
+		Main T = new Main();
+		Scanner kb = new Scanner(System.in);
+		String a=kb.next();
+		String b=kb.next();
+		System.out.print(T.solution(a, b));
 	}
 }
 
