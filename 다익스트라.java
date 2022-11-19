9516e40c80816f43ed712820828ec006382b720b
lusate <95400441+lusate@users.noreply.github.com>
2022-11-19 16:36:56 +0900

    Update 다익스트라.java

--- 다익스트라.java
+++ 다익스트라.java
@@ -1,4 +1,90 @@
 import java.util.*;
+class Edge implements Comparable<Edge>{
+	public int vex, cost;
+	Edge(int vex, int cost){
+		this.vex = vex;
+		this.cost = cost;
+	}
+	@Override
+	public int compareTo(Edge ob){
+		return this.cost - ob.cost;
+	}
+}
+class Main{
+	static ArrayList<ArrayList<Edge>> graph;
+	static int[] dis;
+	static int n,m;
+	public void solution(int v){
+		PriorityQueue<Edge> pQ = new PriorityQueue<>();
+		pQ.offer(new Edge(v, 0));
+		dis[v] = 0;
+		while(!pQ.isEmpty()){
+			Edge tmp = pQ.poll();
+			int now = tmp.vex;
+			int nowCost = tmp.cost;
+			if(nowCost > dis[now]) continue;
+			for(Edge ob : graph.get(now)){
+				if(dis[ob.vex] > nowCost + ob.cost){
+					dis[ob.vex] = nowCost + ob.cost;
+					pQ.offer(new Edge(ob.vex, nowCost+ob.cost));
+				}
+			}
+		}
+	}
+	public static void main(String[] args){
+		Main T = new Main();
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		m = sc.nextInt();
+		
+		graph = new ArrayList<ArrayList<Edge>>();
+		for(int i=0; i<=n; i++){
+			graph.add(new ArrayList<Edge>());
+		}
+		dis = new int[n+1];
+		Arrays.fill(dis, Integer.MAX_VALUE);
+		for(int i=0; i<m; i++){
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			int c = sc.nextInt();
+			graph.get(a).add(new Edge(b, c));
+		}
+
+		T.solution(1);
+		for(int i=2; i<=n; i++){
+			if(dis[i] != Integer.MAX_VALUE) System.out.println(i + " : " + dis[i]);
+			else System.out.println(i + " : impossible");
+		}
+	}
+}
+
+
+/* 입력
+6 9
+1 2 12
+1 3 4
+2 1 2
+2 3 5
+2 5 5
+3 4 5
+4 2 2
+4 5 5
+6 4 5
+*/
+
+/* 출력
+2 : 11
+3 : 4
+4 : 9
+5 : 14
+6 : impossible
+*/
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+import java.util.*;
 //(1,0), (2, 12).... 으로 1번 정점 가는데 드는 비용 0, 2번 정점 가는데 드는 비용 12 
 //이런식으로 만들기 위해서 Edge 클래스를 생성
 class Edge implements Comparable<Edge>{
@@ -54,6 +140,13 @@ class Main {
 			{2, 1, 2}, {2, 3, 5}, {2, 5, 5}, {3, 4, 5}, {4, 2, 2}, {4, 5, 5}, {6, 4, 5}}, 5));
 	}
 }
+/* 입력
+1번 정점에서 출발해서 5번 정점으로 가는데
+최소 비용
+*/
 
+/* 출력
+14
+*/
 
 //다익스트라 : 특정한 하나의 정점에서 다른 모든 정점으로 가는 최단 경로
