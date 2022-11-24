a2df99e52c1abca02054e2e1d3a9e25b0c2dc3a7
lusate <95400441+lusate@users.noreply.github.com>
2022-11-24 14:03:12 +0900

    Create 이중 최단 경로(싸피).java

---
+++ 이중 최단 경로(싸피).java
@@ -0,0 +1,69 @@
+import java.util.*;
+ 
+class Solution {
+    static int N,M;
+    static int[] dis;
+    static ArrayList<int[]>[] graph;
+ 
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int Tc = sc.nextInt();
+        
+        for(int test_case=1; test_case<=Tc; test_case++){
+            N = sc.nextInt();
+            M = sc.nextInt();
+            graph = new ArrayList[N+1];
+            for (int i = 0; i <= N; i++) {
+				graph[i] = new ArrayList<>();
+			}
+ 
+            dis = new int[N+1];
+            Arrays.fill(dis, Integer.MAX_VALUE);
+            for(int i=0; i<M; i++){
+                int a = sc.nextInt();
+                int b = sc.nextInt();
+                int x = sc.nextInt();
+                int y = sc.nextInt();
+ 
+                graph[a].add(new int[]{b, x, y});
+                graph[b].add(new int[]{a, x, y});
+            }
+ 
+            solution();
+ 
+            if(dis[2]!=Integer.MAX_VALUE){
+				System.out.println("#" + test_case + " " + dis[2]);
+			}
+			else System.out.println("#" + test_case + " -1");
+        }
+    }
+ 
+    private static void solution() {
+		//다익스트라
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o[2]));
+
+        for(int[] next : graph[1]){
+			//System.out.println(next[0]); //2, 3
+			//System.out.println(next[1]); //9, 1
+			//System.out.println(next[2]); //9, 1
+            if(dis[next[0]]>next[1]*next[2]){
+                pq.add(new int[]{next[0],next[1], next[2]});
+                dis[next[0]] = next[1]*next[2];
+            }
+        }
+ 
+        while (!pq.isEmpty()) {
+            int[] current = pq.poll();
+ 
+            for(int[] next : graph[current[0]]){
+                int sumX = current[1] + next[1];
+                int sumY = current[2] + next[2];
+ 
+                if(dis[next[0]] > sumX*sumY){
+                    pq.add(new int[]{next[0], sumX, sumY});
+                    dis[next[0]] = sumX*sumY;
+                }
+            }
+        }
+    }
+}
