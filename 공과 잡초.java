0cd8ebc7b567e367ac3f32132ff7f936bad370f7
lusate <95400441+lusate@users.noreply.github.com>
2022-11-25 18:56:25 +0900

    Update 공과 잡초.java

--- 공과 잡초.java
+++ 공과 잡초.java
@@ -1,36 +1,36 @@
 import java.util.*;
 class Solution {
- 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
         for (int test_case = 1; test_case <= T; test_case++) {
-            String str = sc.next();
-
-			System.out.println("#" + test_case + " " + solution(str));
+            String s = sc.next();
+			
+			System.out.println("#" + test_case + " " + solution(s));
         }
     }
  
-    private static int solution(String str) {
-        char[] chars = str.toCharArray();
+    private static int solution(String s) {
+        char[] chars = s.toCharArray();
         Stack<Character> stack = new Stack<>();
-        int result = 0;
+        int cnt = 0;
  
         boolean flag = false;
         for (char ch : chars) {
             if (flag) {
-                if (ch == ')' || ch == '|') result++;
+                if (ch == ')' || ch == '|') cnt++;
                 flag = false;
             } 
 			else if (ch == '(') flag = true;
             
 			else if (ch == ')' && (stack.peek() == '|' || stack.peek() == '(')) {
-                result++;
+                cnt++;
             }
             
 			stack.push(ch);
         }
-        return result;
+
+		return cnt;
     }
 }
 
