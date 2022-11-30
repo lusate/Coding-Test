c4d3f1459042c17ab3ff5cf88040de7c8bfb0462
lusate <95400441+lusate@users.noreply.github.com>
2022-11-30 12:12:07 +0900

    Update 성곽.java

--- 성곽.java
+++ 성곽.java
@@ -12,14 +12,16 @@ class Main {
 	static boolean[][] visit;
 	static int[] dx = {0, -1, 0, 1}; //서북동남 순서대로.
 	static int[] dy = {-1, 0, 1, 0};
+	static int[] dis = {1,2,4,8};
 	static int room, maxRoom, wallRoom = 0;
-	static int[][][] wallCount;
+	static int[][][] wallCount; // [0] 방의 id 저장, [1] 이어진 방의 개수.
+	//벽을 id 기준으로 나눈 결과와 벽을 이어진 칸수로 나눈 결과를 알 수 있다.
 	private static void bfs(int x, int y, int id){
 		Queue<Point> Q = new LinkedList<>();
 		Q.offer(new Point(x, y));
 		visit[x][y] = true;
-		ArrayList<Point> back = new ArrayList<>(); //?
-		int tempRoom=0; //?
+		ArrayList<Point> back = new ArrayList<>(); // 현재 위치의 좌표들을 저장.
+		int tempRoom=0; // 방 개수로 이동할 때마다 1씩 증가시킨다.
 		
 		while(!Q.isEmpty()){
 			Point tmp = Q.poll();
@@ -29,7 +31,12 @@ class Main {
 			for(int i=0; i<4; i++){
 				int nx = tmp.x + dx[i];
 				int ny = tmp.y + dy[i];
-				if((board[tmp.x][tmp.y] & (1 << i)) == 0){ // 벽이 없는 경우
+				// << 는 시프트 연산으로 왼쪽으로 비트를 옮긴다.
+				// (1 << i) 를 하게 되면 1,2,4,8이 된다.
+				// &는 두 비트가 모두 1일 때 1을 반환.
+				if((board[tmp.x][tmp.y] & dis[i]) == 0){ //벽이 없은 경우.
+					// ex) board[0][0] 은 11로 벽이 있는 방향은 1,2,8 이다. 값이 0이 되는 경우는
+					// (1 << i) 가 4인 경우이다. 즉 4 방향인 동쪽이 벽이 없다.
 					if(nx >= 0 && nx < m && ny >= 0 && ny < n && !visit[nx][ny]){
 						visit[nx][ny] = true;
 						Q.offer(new Point(nx, ny));
@@ -37,11 +44,17 @@ class Main {
 				}
 			}
 
-			maxRoom = Math.max(maxRoom, tempRoom);
+			maxRoom = Math.max(maxRoom, tempRoom); //가장 넓은 방의 넓이.
 			for(Point P : back){
-				wallCount[P.x][P.y][1] = tempRoom;
 				wallCount[P.x][P.y][0] = id;
+				wallCount[P.x][P.y][1] = tempRoom;
 			}
+			/* id를 기준으로 나눈 결과           벽을 이어진 칸수로 나눈 결과
+			 * 0 0 1 1 2 2 2					9 9 3 3 8 8 8
+			 * 0 0 0 1 2 3 2					9 9 9 3 8 1 8
+			 * 0 0 0 4 2 4 2					9 9 9 7 8 7 8
+			 * 0 4 4 4 4 4 2					9 7 7 7 7 7 8
+			 */
 		}
 	}
 	private static void MaxRoom(){
@@ -51,8 +64,10 @@ class Main {
 					int nx = i + dx[d];
 					int ny = j + dy[d];
 
-					if(nx>=0 && nx<m && ny>=0 && ny<n && wallCount[i][j][0] != wallCount[nx][ny][0]){
+					if(nx>=0 && nx<m && ny>=0 && ny<n && wallCount[i][j][0] != wallCount[nx][ny][0]){ //현재 위치와 이동한 위치가 같지 않음
+						//같지 않다는 것은 벽이 있다는 것.
 						wallRoom = Math.max(wallRoom, wallCount[i][j][1] + wallCount[nx][ny][1]);
+						//그 2개의 위치를 더해준다. 그 때 최대가 나오는 값이 하나의 벽을 제거하여 얻을 수 있는 가장 넓은 방의 크기.
 					}
 				}
 			}
@@ -76,7 +91,8 @@ class Main {
 		for(int i=0; i<m; i++){
 			for(int j=0; j<n; j++){
 				if(!visit[i][j]){
-					bfs(i, j, room);
+					bfs(i, j, room); //이동할 수 있는 벽이 없을 때까지 이동하고 나서
+					//이동할 수 없게되면 room++;
 					room++;
 				}
 			}
@@ -90,6 +106,7 @@ class Main {
 }
 
 
+
 /* 입력
 7 4
 11 6 11 6 3 10 6
@@ -97,6 +114,7 @@ class Main {
 1 10 12 7 13 7 5
 13 11 10 8 10 12 13
 */
+
 
 /* 출력
 5
