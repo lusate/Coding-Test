69ddb364caebf2eb750aba9dc19dcbcd93e9caf6
lusate <95400441+lusate@users.noreply.github.com>
2022-12-18 21:46:47 +0900

    Update 안전 영역(dfs).java

--- 안전 영역(dfs).java
+++ 안전 영역(dfs).java
@@ -7,7 +7,7 @@ public class Main {
 	static int[] dx = {1, 0, -1, 0};
 	static int[] dy = {0,- 1, 0, 1};
 	private static void dfs(int x, int y, int height) {
-		visit[x][y] = true;
+		visit[x][y] = true; //헌재 지점 
 		for(int i=0; i<4; i++){
 			int nx = x + dx[i];
 			int ny = y + dy[i];
@@ -29,18 +29,21 @@ public class Main {
 		for(int i=0; i<n; i++){
 			for(int j=0; j<n; j++){
 				map[i][j] = sc.nextInt();
+				// 높이는 1이상 100 이하의 정수이다. -> 모든 지역이 다 잠기는 경우도 존재
 				maxHeight = Math.max(maxHeight, map[i][j]);
 			}
 		}
 
 		int answer = 0;
+		//높이를 몇으로 정할 때의 경우의 수를 모두 고려해야 함.
+		//즉 높이가 0 ~ 최대 높이 이하인 지점이 물에 잠기는 경우. -> maxHeight라면 모든 지역이 물에 잠김.
 		for(int height=0; height<=maxHeight; height++){
 			int cnt = 0;
 			visit = new boolean[n][n];
 			for(int i=0; i<n; i++) {
 				for(int j=0; j<n; j++) {
 					if(map[i][j] >= height && !visit[i][j]){
-						cnt++;
+						cnt++; //안전 지역 카운트
 						dfs(i, j, height);
 					}
 				}
