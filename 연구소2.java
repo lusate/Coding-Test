760c6175157651948e5446b1906977413109bc32
lusate <95400441+lusate@users.noreply.github.com>
2023-01-01 20:47:00 +0900

    Update 연구소2.java

--- 연구소2.java
+++ 연구소2.java
@@ -1,102 +1,99 @@
 import java.util.*;
-class Point{
-	int x,y,time;
-	Point(int x, int y, int time){
-		this.x=x;
-		this.y=y;
-		this.time=time;
+class Point {
+	int x, y, time;
+    public Point(int x, int y){
+        this.x=x;
+        this.y=y;
+    }
+
+	public Point(int x, int y, int time) {
+		this.x = x;
+		this.y = y;
+        this.time = time;
 	}
 }
-class Main {
-	static int n, m;
-	static int[][] map, copy;
-	static int min, max;
-	static int[] dx = {-1, 0, 1, 0};
-	static int[] dy = {0, 1, 0, -1};
+public class Main {
+	static int n,m, count = 0, answer = Integer.MAX_VALUE;
+    static Point sel[];
+    static int[][] map;
+    static boolean[][] visit;
+    static ArrayList<Point> arr;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+ 
+    private static void dfs(int idx, int cnt) {
+        if (cnt == m) {
+            bfs();
+            return;
+        }
+ 
+        if(idx == arr.size()){
+            return;
+        }
+        
+        sel[cnt] = arr.get(idx);
+        dfs(idx+1, cnt+1);
+        dfs(idx+1, cnt);
+    }
+ 
+    private static void bfs() {
+        int zero = count;
+        Queue<Point> Q = new LinkedList<>();
 
-	private static void dfs(int cnt){ //바이러스 위치.
-		if(cnt == m){
-			bfs();
-			return;
-		}
-		else{
-			for(int i=0; i<n; i++){
-				for(int j=0; j<n; j++){
-					if(map[i][j] == 2){ //2면 바이러스 설치.
-						map[i][j] = 0; //0으로 설치
-						dfs(cnt+1);
-						map[i][j] = 2; //다시 2로 빽
-					}
-				}
-			}
-		}
-	}
+        for(int i=0; i<m; i++){
+            visit[sel[i].x][sel[i].y] = true;
+			Q.offer(new Point(sel[i].x, sel[i].y, 0));
+        }
 
-	private static void bfs() { //바이러스 퍼짐
-		Queue<Point> Q = new LinkedList<>();
-		//원본이 아닌 바이러스가 퍼지는 copy로 bfs 돌림.
-		for(int i=0; i<n; i++){
-			for(int j=0; j<n; j++){
-				copy[i][j] = map[i][j];
-			}
-		}
+        int time = 0;
+        while(!Q.isEmpty()){
+            Point tmp = Q.poll();
+            time = tmp.time;
+            for(int i=0; i<4; i++){
+                int nx = tmp.x + dx[i];
+                int ny = tmp.y + dy[i];
 
-		//바이러스 삽입.
-		for(int i=0; i<n; i++){
-			for(int j=0; j<n; j++){
-				Q.offer(new Point(i,j,0));
-			}
-		}
+                if(nx>=0 && nx<n && ny>=0 && ny<n && !visit[nx][ny]){
+                    if(map[nx][ny] != 1){
+                        Q.offer(new Point(nx, ny, tmp.time+1));
+                        visit[nx][ny] = true;
+                    }
 
-		while(!Q.isEmpty()){
-			Point tmp = Q.poll();
-			for(int i=0; i<4; i++){
-				int nx = tmp.x + dx[i];
-				int ny = tmp.y + dy[i];
-				if(nx>=0 && nx<n && ny>=0 && ny<n){
-					//바이러스 퍼짐.
-					if(copy[nx][ny] == 0){ //바이러스 퍼짐
-						copy[nx][ny] = tmp.time + 1;
-					}
-				}
-			}
-		}
-		
-		//copy에서 최대값이 바이러스가 퍼지는 시간.
-		max = Integer.MIN_VALUE;
-		for(int i=0; i<n; i++){
-			for(int j=0; j<n; j++){
-				max = Math.max(max, copy[i][j]);
-			}
-		}
+                    if(map[nx][ny] == 0){
+                        zero--;
+                    }
+                }
+            }
+        }
 
-		//그런데 dfs로 바이러스 위치를 다르게 하여 다른 경우의 copy도 있으므로
-		//따로 모든 경우의 copy 최대값의 최소를 구해줌.
-		min = Integer.MAX_VALUE;
-		for(int i=0; i<n; i++){
-			for(int j=0; j<n; j++){
-				min = Math.min(max, min);
-			}
-		}
-		
-	}
+        if(zero == 0){
+            answer = Math.min(answer, time);
+        }
+        
+    }
 
-	public static void main(String[] args) {
+
+    public static void main(String[] args) throws Exception {
 		Scanner sc = new Scanner(System.in);
-		n = sc.nextInt();
-		m = sc.nextInt();
-		map = new int[n][n];
-		copy = new int[n][n];
+        n = sc.nextInt();
+        m = sc.nextInt();
+ 
+        map = new int[n][n];
+        visit = new boolean[n][n];
+        arr = new ArrayList<>();
 
-		for(int i=0; i<n; i++){
-			for(int j=0; j<n; j++){
-				map[i][j] = sc.nextInt();
-			}
-		}
-
-		dfs(0);
-		System.out.println(min);
-	}
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                map[i][j] = sc.nextInt();
+                if (map[i][j] == 2) arr.add(new Point(i, j)); //현재 바이러스 설치 가능한 위치.
+                if (map[i][j] == 0) count++; // 0 개수.
+            }
+        }
+ 
+        sel = new Point[m];
+        dfs(0, 0);
+        System.out.println((answer == Integer.MAX_VALUE) ? -1 : answer);
+    }
 }
 
 
