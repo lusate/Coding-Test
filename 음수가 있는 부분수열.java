2f7831cc3de20423cfb8fd672d01987799aa9298
lusate <95400441+lusate@users.noreply.github.com>
2023-01-05 16:19:27 +0900

    Create 음수가 있는 부분수열.java

---
+++ 음수가 있는 부분수열.java
@@ -0,0 +1,41 @@
+import java.util.*;
+class Main{
+	private static int solution(int[] nums, int m){
+		int answer = 0;
+		HashMap<Integer, Integer> map = new HashMap<>();
+
+		int sum=0;
+		for(int x : nums){
+			//sum을 키로 해서 카운팅.
+			sum += x;
+
+			if(sum == m){
+				answer++;
+			}
+	
+			if(map.containsKey(sum-m)){
+				answer += map.get(sum-m);
+			}
+
+			map.put(sum, map.getOrDefault(sum, 0)+1);
+		}
+
+		return answer;
+	}
+
+	//투 포인터로는 m의 값이 되기 위해 값이 계속 증가해야 한다는 조건때문에 가능했음.
+	public static void main(String[] args){
+		System.out.println(solution(new int[]{1, 2, 3, -3, 1, 2, 2, -3}, 5));
+		System.out.println(solution(new int[]{1, 2, 3, -3, 1, 2}, 3));
+		System.out.println(solution(new int[]{-1, 0, 1}, 0));
+		System.out.println(solution(new int[]{-1, -1, -1, 1}, 0));
+	}
+}
+
+
+/* 출력
+5
+6
+2
+1
+*/
