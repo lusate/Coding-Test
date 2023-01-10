1f050020bbb729af984405a5c9b49bdae035a4e6
lusate <95400441+lusate@users.noreply.github.com>
2023-01-10 14:55:42 +0900

    Update 가장 많이 사용된 회의실.java

--- 가장 많이 사용된 회의실.java
+++ 가장 많이 사용된 회의실.java
@@ -16,21 +16,22 @@ class Main{
 		for(int x[] : meetings){
 			while(!pq.isEmpty() && pq.peek()[0] <= x[0]){
 				rooms.add(pq.poll()[1]);
-				System.out.println(rooms);
+				//System.out.println(rooms);
 				//방 0과 1을 삽입.
 				//0과 1을 사용하고 난 뒤 빈 방이 되면 다시 삽입.
 			}
 
-			if(!rooms.isEmpty()){ //빈 방이 있음
-				int roomNum = rooms.pollFirst(); //rooms에서 첫 번째 0부터.
+			if(!rooms.isEmpty()){ //방이 비어있지 않으면(rooms에 방이 있으면) 가장 빨리 끝나는 회의를 가져옴.
+				int roomNum = rooms.pollFirst(); //rooms에서 첫 번째 0번 방을 가져옴
 				res[roomNum]++; //방 번호 카운트
 				pq.add(new int[]{x[1], roomNum}); //x의 끝나는 시간, room 번호
 			}
-			else { //빈 방이 없으면 가장 빨리 끝나는 회의를 가져옴.
+			else { //방이 비어있지 않으면 -> rooms에 방이 없으면
 				int[] tmp = pq.poll(); // [5, 0]이 빠짐.
 				// tmp[0]은 끝나는 시간, tmp[1]은 방 번호.
 				res[tmp[1]]++; //방 번호 카운트
 				pq.add(new int[]{tmp[0] + (x[1] - x[0]), tmp[1]});
+				//new int[]{회의 사용 시간만큼 tmp[0]에 더해줌, 방 번호}
 			}
 
 		}
