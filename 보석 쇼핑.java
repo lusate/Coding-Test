4831baade6e410415f85542e3cb25cfc34b8370f
lusate <95400441+lusate@users.noreply.github.com>
2023-01-12 14:59:24 +0900

    Update 보석 쇼핑.java

--- 보석 쇼핑.java
+++ 보석 쇼핑.java
@@ -1,61 +1,51 @@
 import java.util.*;
-class Main {
-	public int[] solution(String[] gems) {
-		int[] answer = new int[2];
-		HashSet<String> set = new HashSet<>();
+class Solution {
+    private static int[] solution(String[] gems) {
+        int[] answer = new int[2];
+		//배열을 set으로 바꾸기
+		HashSet<String> set = new HashSet<>(Arrays.asList(gems));
 		HashMap<String, Integer> map = new HashMap<>();
 
 		for(int i=0; i<gems.length; i++){
 			set.add(gems[i]);
 		}
+		System.out.println(set);
 
-		int start = 0;
-		int tempStart = 0;
-		int n = gems.length;
-		
-		Queue<String> Q = new LinkedList<>();
-		for(int i=0; i<gems.length; i++){
-			map.put(gems[i], map.getOrDefault(gems[i], 0)+1);
-			
-			Q.offer(gems[i]);
-			while(!Q.isEmpty()){
-				String gem = Q.peek();
-				if(map.get(gem) > 1){// 중복이 있는 RUBY와 DIA
-					map.put(gem, map.get(gem) - 1); //중복인 key의 value는 1감소
-					Q.poll(); //Q에서 제거
-					tempStart++; //start 를 뒤로 밀고 감
+		int k = set.size(); //k는 4
+		int min = 1000000;
+		int left=0;
+		for(int right=0; right<gems.length; right++){
+			map.put(gems[right], map.getOrDefault(gems[right], 0) + 1);
+
+			while(map.size() == k){
+				if(right - left + 1 < min){ //길이가 최소가 되도록.
+					min = right - left + 1;
+					answer = new int[]{left+1, right+1}; //인덱스를 1부터 시작했기 때문.
 				}
-				else break;
+
+				map.put(gems[left], map.getOrDefault(gems[left], 0) - 1);
+
+				if(map.get(gems[left]) == 0){ //개수가 0이 되면 remove로 삭제해서 while 돌림.
+					map.remove(gems[left]);
+				}
+
+				left++;
 			}
-			
-			if(map.size() == set.size()){//map 크기와 set 크기가 같다면
-				if(n > Q.size()){ // 모든 보석이 Q에 한 번 이상씩 포함되었다는 의미
-					n = Q.size(); //현재 모든 보석이 포함된 길이(n) 보다 Q의 크기가 더 작은지 확인
-					start = tempStart; //더 작으면 Q의 크기를 n으로 지정해주고 시작 지점을 재설정
-				}
-			}//n의 크기가 더 크면 위 연산 다시 반복
 		}
-		answer = new int[]{start + 1, start + n};
+
+
 		return answer;
-	}
-		
+    }
+
 	public static void main(String[] args){
-		Main T = new Main();
-		String[] gems1 = new String[]{"DIA", "RUBY", "RUBY","DIA","DIA","EMERALD","SAPPHIRE","DIA"};
-		for(int x : T.solution(gems1)){
-			System.out.print(x + " ");
-		}
-		//System.out.println();
-
-		//String[] gems2 = new String[]{"AA", "AB", "AC","AA","AC"};
-		//for(int x : T.solution(gems2)){
-		//	System.out.print(x + " ");
-		//}
-		//System.out.println();
-
-		//String[] gems3 = new String[]{"XYZ", "XYZ", "XYZ"};
-		//for(int x : T.solution(gems3)){
-		//	System.out.print(x + " ");
-		//}
+		System.out.println(Arrays.toString(solution(new String[]{"DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"})));
+		//System.out.println(Arrays.toString(solution(new String[]{"AA", "AB", "AC", "AA", "AC"})));
+		//System.out.println(Arrays.toString(solution(new String[]{"XYZ", "XYZ", "XYZ"})));
 	}
 }
+
+/* 출력
+[3, 7]
+[1, 3]
+[1, 1]
+*/
