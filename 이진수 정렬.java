b1a3b34df6448c020c5b68b6b8fed752c7af28b5
lusate <95400441+lusate@users.noreply.github.com>
2023-01-30 20:22:26 +0900

    Update 이진수 정렬.java

--- 이진수 정렬.java
+++ 이진수 정렬.java
@@ -30,7 +30,14 @@ class Main{
 	
 	public static void main(String[] args){
 		System.out.println(Arrays.toString(solution(new int[]{5, 6, 7, 8, 9})));
-		//System.out.println(Arrays.toString(solution(new int[]{5,4,3,2,1})));
-		//System.out.println(Arrays.toString(solution(new int[]{12, 5, 7, 23, 45, 21, 17})));
+		System.out.println(Arrays.toString(solution(new int[]{5,4,3,2,1})));
+		System.out.println(Arrays.toString(solution(new int[]{12, 5, 7, 23, 45, 21, 17})));
 	}
 }
+
+
+/* 출력
+[8, 5, 6, 9, 7]
+[1, 2, 4, 3, 5]
+[5, 12, 17, 7, 21, 23, 45]
+*/
