d08428459fe43f2a3c18ff8c2b31e3dc91800fc4
lusate <95400441+lusate@users.noreply.github.com>
2023-02-02 17:34:23 +0900

    Create 미로 탈출.java

---
+++ 미로 탈출.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution{
+		public int solution(int n, int start, int end, int[][] roads, int[] traps){
+		int answer = 0;
+
+		return answer;
+    }
+	public static void main(String[] args){
+		Solution T = new Solution();
+		System.out.println(T.solution(3, 1, 3, new int[][]{{1,2,2}, {3,2,3}}, new int[]{2}));
+		System.out.println(T.solution(4, 1, 4, new int[][]{{1,2,1}, {3,2,1}, {2,4,1}}, new int[]{2,3}));
+	}
+}
+
+
+/* 출력
+5
+4 
+*/
