2d7ec74310a5d78abbe6d058867eca3501dc4d2f
lusate <95400441+lusate@users.noreply.github.com>
2023-02-02 23:58:39 +0900

    Update 미로 탈출.java

--- 미로 탈출.java
+++ 미로 탈출.java
@@ -1,4 +1,35 @@
 import java.util.*;
+class Edge {
+    int vertex;
+    int weight;
+    boolean isReverse;
+
+    public Edge(int vertex, int weight, boolean isReverse) {
+        this.vertex = vertex;
+        this.weight = weight;
+        this.isReverse = isReverse;
+    }
+}
+class Node implements Comparable<Node> {
+    // 현재 노드
+    int vertex;
+    // 시작 ~ 현재 노드의 경로
+    int weight;
+    // 현재 트랩의 활성 상태
+    int trapStatus;
+
+    public Node(int vertex, int weight, int trapStatus) {
+        this.vertex = vertex;
+        this.weight = weight;
+        this.trapStatus = trapStatus;
+    }
+
+    @Override
+    public int compareTo(Node ob) {
+        return this.weight > ob.weight ? 1 : -1;
+    }
+}
+
 class Solution{
 		public int solution(int n, int start, int end, int[][] roads, int[] traps){
 		int answer = 0;
