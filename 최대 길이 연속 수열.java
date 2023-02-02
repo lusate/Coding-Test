d517505d62f9e7b9a8ab7b90c21eab3699100cd5
lusate <95400441+lusate@users.noreply.github.com>
2023-02-02 18:32:15 +0900

    Update 최대 길이 연속 수열.java

--- 최대 길이 연속 수열.java
+++ 최대 길이 연속 수열.java
@@ -25,7 +25,9 @@ class Solution {
 	public static void main(String args[]){
 		Solution T = new Solution();
 		System.out.println(T.solution(new int[]{8, 1, 9, 3, 10, 2, 4, 0, 2, 3}));
-
+		System.out.println(T.solution(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 0, 0, 0, 0}));
+		System.out.println(T.solution(new int[]{3, 3, 3, 3, 3, 3, 3, 3}));
+		System.out.println(T.solution(new int[]{-5, -3, -1, -4, 3, 3, 5, 6, 2, 2, 1, 1, 7}));
 	}
 }
 
