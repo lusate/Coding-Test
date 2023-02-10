4d55ace574571d6415422c326cb5c5fcb50a5b03
lusate <95400441+lusate@users.noreply.github.com>
2023-02-10 17:58:15 +0900

    Update 메뉴 리뉴얼.java

--- 메뉴 리뉴얼.java
+++ 메뉴 리뉴얼.java
@@ -68,9 +68,9 @@ class Solution {
 
 	public static void main(String args[]){
 		Solution T = new Solution();
-		// System.out.println(Arrays.toString(T.solution(new String[]{"ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"}, new int[]{2,3,4})));
+		System.out.println(Arrays.toString(T.solution(new String[]{"ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"}, new int[]{2,3,4})));
 		System.out.println(Arrays.toString(T.solution(new String[]{"ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"}, new int[]{2,3,5})));
-		// System.out.println(Arrays.toString(T.solution(new String[]{"XYZ", "XWY", "WXA"}, new int[]{2,3,4})));
+		System.out.println(Arrays.toString(T.solution(new String[]{"XYZ", "XWY", "WXA"}, new int[]{2,3,4})));
 	}
 }
 
