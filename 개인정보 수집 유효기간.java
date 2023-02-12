2a31b257fa9bcddf75d85bc078fdf55ba37ffddc
lusate <95400441+lusate@users.noreply.github.com>
2023-02-12 20:20:53 +0900

    Update 개인정보 수집 유효기간.java

--- 개인정보 수집 유효기간.java
+++ 개인정보 수집 유효기간.java
@@ -67,9 +67,9 @@ class Solution {
 		new String[]{"2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"})));
 
 
-		// System.out.println(Arrays.toString(T.solution("2020.01.01", 
-		// new String[]{"Z 3", "D 5"}, 
-		// new String[]{"2019.01.01 D", "2019.11.15 Z", "2019.08.02 D", "2019.07.01 D", "2018.12.28 Z"})));
+		System.out.println(Arrays.toString(T.solution("2020.01.01", 
+		new String[]{"Z 3", "D 5"}, 
+		new String[]{"2019.01.01 D", "2019.11.15 Z", "2019.08.02 D", "2019.07.01 D", "2018.12.28 Z"})));
 	}
 }
 
