8acbdc12e61a6e1918cce4ecf6a357ea637e2437
lusate <95400441+lusate@users.noreply.github.com>
2023-02-16 21:39:15 +0900

    Update 주차 요금 계산.java

--- 주차 요금 계산.java
+++ 주차 요금 계산.java
@@ -1,4 +1,94 @@
 import java.util.*;
+class Solution {
+    private static int getTime(String time){
+		int H = Integer.parseInt(time.split(":")[0]);
+		int M = Integer.parseInt(time.split(":")[1]);
+
+		return 60*H+M;
+	}
+    
+    public int[] solution(int[] fees, String[] records) {
+		int[] map = new int[10001]; // 입차 시간. IN인 버스들
+		int[] sum = new int[10001]; // OUT - IN 시간 계산
+		int[] visit = new int[10001]; // IN만 있는 경우를 찾기 위해 버스 번호를 인덱스로 저장.
+
+		for(String x : records){
+			String a = x.split(" ")[0];
+			String b = x.split(" ")[1];
+			String c = x.split(" ")[2];
+
+			int num = Integer.parseInt(b);
+
+			//map에 IN 넣기.
+			if(c.equals("IN")){
+				map[num] = getTime(a);
+				
+				visit[num] = 1;
+			}
+			else{
+				sum[num] = getTime(a) - map[num] + sum[num];
+				visit[num] = 0;
+			}
+		}
+		
+
+		ArrayList<Integer> res = new ArrayList<>();
+		for(int i=0; i<10001; i++){
+			// 0000이 1로 OUT하지 않고 남아있음
+			if(visit[i] == 1) sum[i] += 23*60+59 - map[i];
+
+			if(sum[i] > 0){
+				res.add(sum[i]);
+			}
+		}
+
+
+		// int[] answer = new int[res.size()];
+		// for(int i=0; i<res.size(); i++){
+		// 	answer[i] = res.get(i);
+
+		// 	if(answer[i] < fees[0]){
+		// 		answer[i] = fees[1];
+		// 	}
+		// 	else{
+		// 		answer[i] = fees[1] + Math.ceil((answer[i] - fees[0]) / fees[2]) * fees[3];
+		// 	}
+		// }
+
+		int[] answer = new int[res.size()];
+		int idx=0;
+		for(int val : res){
+			answer[idx] = fees[1]; //기본 요금
+			if(val > fees[0]){ //추가 요금
+				answer[idx] += Math.ceil((val - fees[0]) / (double)fees[2]) * fees[3];
+			}
+
+			idx++;
+		}
+
+
+		//answer[idx] += Math.ceil((val - fees[0]) / (double)fees[2]) * fees[3];
+
+        return answer;
+    }
+	
+	public static void main(String[] args){
+		System.out.println(Arrays.toString(solution(new int[]{180, 5000, 10, 600}, 
+		new String[]{"05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT", "07:59 5961 OUT", 
+		"07:59 0148 IN", "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", 
+		"23:00 5961 OUT"})));
+
+		System.out.println(Arrays.toString(solution(new int[]{120, 0, 60, 591}, 
+		new String[]{"16:00 3961 IN","16:00 0202 IN","18:00 3961 OUT","18:00 0202 OUT","23:58 3961 IN"})));
+
+		System.out.println(Arrays.toString(solution(new int[]{1, 461, 1, 10}, 
+		new String[]{"00:00 1234 IN"})));
+	}
+}
+
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------
+import java.util.*;
 class Main{
 	private static int getTime(String time){
 		int H = Integer.parseInt(time.split(":")[0]);
@@ -67,11 +157,11 @@ class Main{
 		"07:59 0148 IN", "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", 
 		"23:00 5961 OUT"})));
 
-		//System.out.println(Arrays.toString(solution(new int[]{120, 0, 60, 591}, 
-		//new String[]{"16:00 3961 IN","16:00 0202 IN","18:00 3961 OUT","18:00 0202 OUT","23:58 3961 IN"})));
+		System.out.println(Arrays.toString(solution(new int[]{120, 0, 60, 591}, 
+		new String[]{"16:00 3961 IN","16:00 0202 IN","18:00 3961 OUT","18:00 0202 OUT","23:58 3961 IN"})));
 
-		// System.out.println(Arrays.toString(solution(new int[]{1, 461, 1, 10}, 
-		// new String[]{"00:00 1234 IN"})));
+		System.out.println(Arrays.toString(solution(new int[]{1, 461, 1, 10}, 
+		new String[]{"00:00 1234 IN"})));
 	}
 }
 
