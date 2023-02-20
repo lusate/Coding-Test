a0dc35a572abc13d00366779719f88f20d7666df
lusate <95400441+lusate@users.noreply.github.com>
2023-02-20 17:40:38 +0900

    Create 외톨이 알파벳.java

---
+++ 외톨이 알파벳.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution{
+	public String solution(String input_string) {
+		String answer = "";
+
+		return answer;
+	}
+
+	public static void main(String[] args){
+		Solution T = new Solution();
+		System.out.println(T.solution("edeaaabbccd"));
+		System.out.println(T.solution("eeddee"));
+		System.out.println(T.solution("string"));
+		System.out.println(T.solution("zbzbz"));
+	}
+}
+
+
+/* 출력
+"de"
+"e"
+"N"
+"bz"
+*/
