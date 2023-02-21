5fb8de8c6be3887a6424769d9efcde83c645fe44
lusate <95400441+lusate@users.noreply.github.com>
2023-02-21 21:46:08 +0900

    Update 양과 늑대.java

--- 양과 늑대.java
+++ 양과 늑대.java
@@ -50,6 +50,16 @@ class Solution {
         answer = maxSheep;
         return answer;
     }
+	
+	public static void main(String[] args){
+		Solution T = new Solution();
+		System.out.println(T.solution(new int[]{0,0,1,1,1,0,1,0,1,0,1,1}, 
+        new int[][]{{0, 1}, {1, 2}, {1, 4}, {0, 8}, {8, 7}, {9, 10}, 
+        {9, 11}, {4, 3}, {6, 5}, {4, 6}, {8, 9}}));
+
+        System.out.println(T.solution(new int[]{0,1,0,1,1,0,1,0,0,1,0},
+        new int[][]{{0, 1}, {0, 2}, {1, 3}, {1, 4}, {2, 5}, {2, 6}, {3, 7}, {4, 8}, {6, 9}, {9, 10}}));
+	}
 }
 
 
@@ -105,4 +115,14 @@ class Solution {
         
         return answer;
     }
+	
+	public static void main(String[] args){
+		Solution T = new Solution();
+		System.out.println(T.solution(new int[]{0,0,1,1,1,0,1,0,1,0,1,1}, 
+        new int[][]{{0, 1}, {1, 2}, {1, 4}, {0, 8}, {8, 7}, {9, 10}, 
+        {9, 11}, {4, 3}, {6, 5}, {4, 6}, {8, 9}}));
+
+        System.out.println(T.solution(new int[]{0,1,0,1,1,0,1,0,0,1,0},
+        new int[][]{{0, 1}, {0, 2}, {1, 3}, {1, 4}, {2, 5}, {2, 6}, {3, 7}, {4, 8}, {6, 9}, {9, 10}}));
+	}
 }
