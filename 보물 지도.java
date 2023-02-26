e6cae9e460378cac4f60ce1a0514f7f8af547738
lusate <95400441+lusate@users.noreply.github.com>
2023-02-26 16:53:41 +0900

    Update 보물 지도.java

--- 보물 지도.java
+++ 보물 지도.java
@@ -1,51 +1,43 @@
 import java.util.*;
-class Node{
-	int n, m, time, jumpCount;
-
-	public Node(int n, int m, int time, int jumpCount) {
-		this.n = n;
-		this.m = m;
-		this.time = time;
-		this.jumpCount = jumpCount;
-	}
-}
 class Solution {
 	int[] dx = {0, 1, 0, -1};
     int[] dy = {-1, 0, 1, 0};
     public int solution(int n, int m, int[][] hole) {
-		boolean[][][] visit = new boolean[n][m][2];
-        int[][] map = new int[n][m];
-
-		for (int[] data : hole) {
-            map[data[0] - 1][data[1] - 1] = 1;
-        }
+		int[][] map = new int[n][m];
+		boolean[][][] visit = new boolean[n][m][2]; //2는 2점프 횟수.
+		
+		for(int[] x : hole){
+			map[x[0] - 1][x[1] - 1] = 1;
+		}
 
 		visit[0][0][1] = true;
 		int answer = Integer.MAX_VALUE;
-		Queue<Node> q = new LinkedList<>();
-        q.add(new Node(0, 0, 0, 1));
+
+		Queue<int[]> q = new LinkedList<>();
+		q.offer(new int[]{0, 0, 0, 1});
 		while(!q.isEmpty()){
-			Node tmp = q.poll();
-			if(tmp.n == n-1 && tmp.m == m-1) answer = Math.min(answer, tmp.time);
+			int[] tmp = q.poll();
+
+			if(tmp[0] == n-1 && tmp[1] == m-1) answer = Math.min(answer, tmp[2]);
 
 			for(int k=0; k<4; k++){
-				int nx = tmp.n + dx[k];
-				int ny = tmp.m + dy[k];
+				int nx = tmp[0] + dx[k];
+				int ny = tmp[1] + dy[k];
 
-				if(nx>=0 && nx<n && ny>=0 && ny<m && map[nx][ny] != 1 && !visit[nx][ny][tmp.jumpCount]){
-					visit[nx][ny][tmp.jumpCount] = true;
-					q.offer(new Node(nx, ny, tmp.time + 1, tmp.jumpCount));
+				if(nx>=0 && ny>=0 && nx<n && ny<m && map[nx][ny] == 0 && !visit[nx][ny][tmp[3]]){
+					visit[nx][ny][tmp[3]] = true;
+					q.offer(new int[]{nx, ny, tmp[2]+1, tmp[3]});
 				}
 			}
 
-			if(tmp.jumpCount > 0){
+			//점프 횟수를 1로 해야하는데 처음에 q에 {0, 0, 0, 0}을 넣으면 점프하려는 개수를 늘릴 수 없어서 처음에 {0, 0, 0, 1}로 잡음.
+			if(tmp[3] > 0){
 				for(int k=0; k<4; k++){
-					int nx = tmp.n + dx[k] * 2;
-					int ny = tmp.m + dy[k] * 2;
-	
-					if(nx>=0 && nx<n && ny>=0 && ny<m && map[nx][ny] != 1 && !visit[nx][ny][tmp.jumpCount - 1]){
-						visit[nx][ny][tmp.jumpCount - 1] = true;
-						q.offer(new Node(nx, ny, tmp.time + 1, tmp.jumpCount - 1));
+					int kx = tmp[0] + dx[k] * 2;
+					int ky = tmp[1] + dy[k] * 2;
+					if(kx>=0 && ky>=0 && kx<n && ky<m && map[kx][ky] == 0 && !visit[kx][ky][tmp[3] - 1]){ // 점프 부분을 -1해서 한 칸만 움직이도록 함.
+						visit[kx][ky][tmp[3] - 1] = true;
+						q.offer(new int[]{kx, ky, tmp[2] + 1, tmp[3] - 1});
 					}
 				}
 			}
