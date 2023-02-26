7aa6e310a30adaecd5de974c804fc2dee9a2413b
lusate <95400441+lusate@users.noreply.github.com>
2023-02-26 23:55:26 +0900

    Update 청소.java

--- 청소.java
+++ 청소.java
@@ -36,6 +36,7 @@ public class Main {
 	}
 
 	public static void main(String[] args) {
+		Solution T = new Solution();
 		int[][] board = new int[][]{{0,0,0,0,0},{0,1,1,0,0},{0,0,0,0,0}, {1,0,1,0,1}, {0,0,0,0,0}};
 		int k = 10;
 
