8e2b9b4abeb269b0aad4eaeee307c359d074fe8a
lusate <95400441+lusate@users.noreply.github.com>
2023-02-28 17:26:59 +0900

    Update 길 찾기 게임.java

--- 길 찾기 게임.java
+++ 길 찾기 게임.java
@@ -1,1 +1,20 @@
+import java.util.*;
+class Solution {
+    public int[][] solution(int[][] nodeinfo) {
+		int[][] answer = {};
+		
+        return answer;
+    }
 
+	public static void main(String[] args){
+		Solution T = new Solution();
+		System.out.println(Arrays.deepToString(T.solution(new int[][]{{5, 3}, {11, 5}, {13, 3}, {3, 5}, {6, 1}, 
+		{1, 3}, {8, 6}, {7, 2}, {2, 2}})));
+		
+	}
+}
+
+
+/* 출력
+[[7,4,6,9,1,8,5,2,3],[9,6,5,8,1,4,3,2,7]]
+*/
