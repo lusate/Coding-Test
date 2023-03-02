a4542b0737527b1e8a60a518093486a6f7df6ce2
lusate <95400441+lusate@users.noreply.github.com>
2023-03-03 00:05:09 +0900

    Update 모임장소.java

--- 모임장소.java
+++ 모임장소.java
@@ -1,6 +1,6 @@
 import java.util.*;
 class Main{
-	private static int solution(int[][] board){
+	public int solution(int[][] board){
 		//이 방법은 장애물이 없는 경우만 가능한 로직.
 		int answer = 0;
 		int n = board.length;
@@ -39,11 +39,11 @@ class Main{
 	}
 	
 	public static void main(String[] args){
-		System.out.println(solution(new int[][]{{1, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 1},
+		System.out.println(T.solution(new int[][]{{1, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 1},
 		{0, 0, 0, 0, 0}, {0, 0, 1, 0, 0}}));
-		System.out.println(solution(new int[][]{{1, 0, 0, 0, 1}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0},
+		System.out.println(T.solution(new int[][]{{1, 0, 0, 0, 1}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0},
 		{0, 0, 0, 0, 0}, {0, 0, 0, 0, 1}}));
-		System.out.println(solution(new int[][]{{1, 0, 0, 0, 1, 1}, {0, 1, 0, 0, 1, 0}, {0, 1, 0, 0, 0, 0},
+		System.out.println(T.solution(new int[][]{{1, 0, 0, 0, 1, 1}, {0, 1, 0, 0, 1, 0}, {0, 1, 0, 0, 0, 0},
 		{0, 0, 0, 0, 1, 0}, {0, 0, 0, 0, 0, 1}, {1, 0, 0, 0, 1, 1}}));
 	}
 }
