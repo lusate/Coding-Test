ec63912a213801e965e1e339d1eac461aa407a7d
sang woo park <95400441+lusate@users.noreply.github.com>
2023-03-09 17:04:34 +0900

    Update 경고 메일.java

--- 경고 메일.java
+++ 경고 메일.java
@@ -1,12 +1,12 @@
 import java.util.*;
-class Main{
-	private static int getTime(String time){
+class Solution{
+	public int getTime(String time){
 		int H = Integer.parseInt(time.split(":")[0]);
 		int M = Integer.parseInt(time.split(":")[1]);
 
 		return 60 * H + M;
 	}
-	private static ArrayList<String> solution(String[] reports, int times){
+	public ArrayList<String> solution(String[] reports, int times){
 		ArrayList<String> answer = new ArrayList<>();
 		HashMap<String, Integer> map = new HashMap<>(); //들어와 있는 시간
 		HashMap<String, Integer> sum = new HashMap<>(); //총 사용 시간.
@@ -40,13 +40,13 @@ class Main{
 	}
 	
 	public static void main(String[] args){
-		System.out.println(solution(new String[]{"john 09:30 in", "daniel 10:05 in", 
+		Solution T = new Solution();
+		System.out.println(T.solution(new String[]{"john 09:30 in", "daniel 10:05 in", 
 		"john 10:15 out", "luis 11:57 in", "john 12:03 in", "john 12:20 out", "luis 12:35 out", 
 		"daniel 15:05 out"}, 60));
 
 
-
-		System.out.println(solution(new String[]{"bill 09:30 in", "daniel 10:00 in", "bill 11:15 out", 
+		System.out.println(T.solution(new String[]{"bill 09:30 in", "daniel 10:00 in", "bill 11:15 out", 
 		"luis 11:57 in", "john 12:03 in", "john 12:20 out", "luis 14:35 out", 
 		"daniel 14:55 out"}, 120));
 	}
