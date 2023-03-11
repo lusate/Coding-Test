42e1e77752a1eaff18761860163ae8e0aacd4497
sang woo park <95400441+lusate@users.noreply.github.com>
2023-03-11 18:02:43 +0900

    Update 할인 행사.java

--- 할인 행사.java
+++ 할인 행사.java
@@ -22,13 +22,13 @@ class Solution {
 				int idx = i+j;
 				String idxKey = discount[idx]; // 10개씩
 
-				if(!tmp.containsKey(idxKey)){
+				if(!tmp.containsKey(idxKey)){ // tmp에 없으면 break;
 					isExist = false;
 					break;
 				}
 
 				int valueCnt = tmp.get(idxKey);
-				if(valueCnt - 1 < 0){
+				if(valueCnt - 1 < 0){ //tmp의 값에서 -1을 했을 때 0보다 작으면 break
 					isExist = false;
 					break;
 				}
@@ -36,7 +36,7 @@ class Solution {
 				tmp.put(idxKey, valueCnt - 1);
 			}
 
-			if(isExist){
+			if(isExist){ // 위 2가지를 제외하고 tmp의 값을 봤더니 0이 없으면 break
 				for(String key : map.keySet()){
 					if(tmp.get(key) != 0){
 						isExist = false;
