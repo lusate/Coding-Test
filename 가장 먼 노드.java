583ded87dbabde553f14376c0875330f88efc66b
sang woo park <95400441+lusate@users.noreply.github.com>
2023-03-14 17:36:31 +0900

    Update 가장 먼 노드.java

--- 가장 먼 노드.java
+++ 가장 먼 노드.java
@@ -59,7 +59,7 @@ class Solution {
     }
 
 	public static void main(String[] args){
-		Main T = new Main();
+		Solution T = new Solution();
 		System.out.println(T.solution(6, new int[][]{{3, 6}, {4,3}, {3,2}, {1,3}, {1,2}, {2,4} ,{5,2}}));
 	}
 }
