8642edb924ea3db08615091c18c6dde0afca5738
sang woo park <95400441+lusate@users.noreply.github.com>
2023-03-18 00:36:57 +0900

    Update 회의실 만남.java

--- 회의실 만남.java
+++ 회의실 만남.java
@@ -14,26 +14,31 @@ class Solution {
 			enterIdx[enter[i]] = i;
 		}
 		
-		int[] enterT = new int[n];
-		int[] exitT = new int[n];
+		int[] enterT = new int[n]; // 들어왔을 때 시간
+		int[] exitT = new int[n]; // 나갈 때 시간
 
 		int cnt = 0;
 		int j = 0;
 		for(int i=0; i<n; i++){
-			while(j < n && j <= enterIdx[exit[i]]){
-				enterT[enter[j]] = cnt++; //들어올 때 시간 +
+			while(j < n && j <= enterIdx[exit[i]]){ // 0번이 나갈려면 1번과 2번이 나가야 함. 그 시간을 구해줌.
+				enterT[enter[j]] = cnt++; // 1번이 나가는 것은 0번 들어오고 1번 들어오고 1번이 나가면 됨.
 				j++;
 			}
 
-			exitT[exit[i]] = cnt++; //들어오자마자 나가도록하고 시간 +
+			exitT[exit[i]] = cnt++; // 나갈 때의 시간을 구해줌.
 		}
+		// enterT[] -> [0, 1, 4, 7, 3]
+		// exitT[] -> [6, 2, 5, 8, 9]
 
 		int[] answer = new int[n];
 		for(int i=0; i<n; i++){
-			for(j = i+1; j<n; j++){
-				if(exitT[i] < enterT[j] || exitT[j] < enterT[i]){
+			for(int k = i+1; k<n; k++){
+				// 두 사람이 만나지 않을려면 i의 나가는 시간이 k의 들어오는 시간보다 빨라야 함.
+				// 반대로 k의 나가는 시간이 i의 들어오는 시간보다 빠르면 만나지 않음.
+				// 그걸 반대로 하면 무조건 만남.
+				if(!(exitT[i] < enterT[k] || exitT[k] < enterT[i])){
 					answer[i]++;
-					answer[j]++;
+					answer[k]++;
 				}
 			}
 		}
