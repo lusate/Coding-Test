c2b604bc9fa8f5b06c94652414f901dd8c3add1e
sang woo park <95400441+lusate@users.noreply.github.com>
2023-03-17 16:36:46 +0900

    Create 징검다리 건너기.java

---
+++ 징검다리 건너기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] stones, int k) {
+        
+    }
+
+	public static void main(String[] args){
+		Solution T = new Solution();
+		System.out.println(Arrays.toString(T.solution(new int[]{2, 4, 5, 3, 2, 1, 4, 2, 5, 1})));
+	}
+}
+
+
+/* 출력
+3
+*/
