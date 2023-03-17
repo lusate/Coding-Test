706185de03e6f5bde3041be025a4f65d8bcd26ce
sang woo park <95400441+lusate@users.noreply.github.com>
2023-03-17 16:38:35 +0900

    Create 불량 사용자.java

---
+++ 불량 사용자.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] stones, int k) {
+        
+    }
+
+	public static void main(String[] args){
+		Solution T = new Solution();
+		System.out.println(T.solution(new String[]{"frodo", "fradi", "crodo", "abc123", "frodoc"}, 
+                                                 new String[]{"fr*d*", "abc1**"}));
+	}
+}
