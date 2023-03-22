98ea19f31fb4435091c6353e2e1914bef97e61f0
sang woo park <95400441+lusate@users.noreply.github.com>
2023-03-22 23:32:28 +0900

    Update 비밀번호.java

--- 비밀번호.java
+++ 비밀번호.java
@@ -56,9 +56,9 @@ class Solution{
 	
 	public static void main(String[] args){
 		Solution T = new Solution();
-        System.out.println(T.solution(new int[]{2, 5, 3, 7, 1, 6, 4, 9, 8}, "7596218"));
-        System.out.println(T.solution(new int[]{1, 5, 7, 3, 2, 8, 9, 4, 6}, "63855526592"));
-        System.out.println(T.solution(new int[]{2, 9, 3, 7, 8, 6, 4, 5, 1}, "323254677"));
+		System.out.println(T.solution(new int[]{2, 5, 3, 7, 1, 6, 4, 9, 8}, "7596218"));
+		System.out.println(T.solution(new int[]{1, 5, 7, 3, 2, 8, 9, 4, 6}, "63855526592"));
+		System.out.println(T.solution(new int[]{2, 9, 3, 7, 8, 6, 4, 5, 1}, "323254677"));
 	}
 }
 
