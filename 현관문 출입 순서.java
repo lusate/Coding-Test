380e8d47832aaec525765e2c620481d57ae12e3c
sang woo park <95400441+lusate@users.noreply.github.com>
2023-03-24 22:45:37 +0900

    Update 현관문 출입 순서.java

--- 현관문 출입 순서.java
+++ 현관문 출입 순서.java
@@ -7,58 +7,48 @@ class Solution {
 		int n = arrival.length;
 		int[] answer = new int[n];
 		
-		int prev = 0; //나가지도 들어오지도 않으면 prev를 -1
+	    	//나가기 위해 현관문 사용하면 1, 들어오기 위해 사용하면 0
+		int prev = 1; //나가지도 들어오지도 않으면 prev를 -1
 		
-		for(int t=0, i=0, cnt=0; ; t++){ //t는 시간
-			if(enterQ.isEmpty() && exitQ.isEmpty()){
-				if(t < arrival[i]){
+		for(int t=0, i=0, cnt=0; ; t++){ //t는 시간, i는 사원 번호, cnt는 현관문 사용한 사람 수
+			//현관문 사용하기 위해 대기하고 있는 사람이 없다는 뜻
+			if(enterQ.isEmpty() && exitQ.isEmpty() && i<n){
+				if(t < arrival[i]){ //현재 시간이 작으면 건너띈다.
 					t = arrival[i]; //비어있으면 초를 바꿈.
-					prev = -1; //들락 날락 안 함 -> 아무일도 안 일어남.
+					prev = 1; 
 				}
 			}
 
 			while(i < n && t >= arrival[i]){ //i가 번호
-				if(state[i] == 0) enterQ.offer(i);
+				if(state[i] == 0) enterQ.offer(i); //들어오면 enterQ에 삽입
 				else exitQ.offer(i);
 				i++;
 			}
 			
-			if(prev == -1){
-				if(!exitQ.isEmpty()){
-					answer[exitQ.poll()] = t;
+			if(prev == 1){ //1초 전에 현관문을 사용하지 않았거나 나가는 사원의 경우
+				if(!exit.isEmpty()){
+					answer[exit.poll()] = t;  // exit.poll()는 사원 번호
 					prev = 1;
-					//1초 전에 현관문을 사용한 적이 없으면 나가는 사원이 먼저 현관문을 이용
 				}
-
-				else if(!enterQ.isEmpty()){ //들어오면 0처리
-					answer[enterQ.poll()] = t;
+				else{ // 나가는 사원이 없음 들어오는 사람이 사용
+					answer[enter.poll()] = t;
 					prev = 0;
 				}
 			}
 
 			else if(prev == 0){
-				if(!enterQ.isEmpty()){ //들어오는 사람 먼저
-					answer[enterQ.poll()] = t;
+				if(!enter.isEmpty()){
+					answer[enter.poll()] = t;
 					prev = 0;
 				}
-				else if(!exitQ.isEmpty()){ //누군가 한 쪽에는 있으므로 굳이 else if 할 필요는 없음.
-					answer[exitQ.poll()] = t;
+				else{
+					answer[exit.poll()] = t;
 					prev = 1;
-				}
-			}
-			else if(prev == 1){
-				if(!exitQ.isEmpty()){
-					answer[exitQ.poll()] = t;
-					prev = 1;
-				}
-				else if(!enterQ.isEmpty()){ //누군가 한 쪽에는 있으므로 굳이 else if 할 필요는 없음.
-					answer[enterQ.poll()] = t;
-					prev = 0;
 				}
 			}
 
 			cnt++;
-			if(cnt == n) break;
+			if(cnt == n) break; //현관문 사용한 사람 수가 n과 같아지면 break
 		}
 		
 		return answer;
