24896c063bac4a1ece58cd5a8f7af65d8f5d0ee3
sang woo park <95400441+lusate@users.noreply.github.com>
2023-03-28 15:03:12 +0900

    Update 징검다리 건너기.java

--- 징검다리 건너기.java
+++ 징검다리 건너기.java
@@ -1,16 +1,33 @@
 import java.util.*;
 class Solution {
-    public int solution(int[] stones, int k) {
-        
+    public int count(int mid, int k, int[] stones){
+        int cnt = 0;
+        for(int i=0; i<stones.length; i++){
+            if(stones[i] < mid){
+                cnt++;
+                if(cnt >= k) break;
+            }
+            else{
+                cnt=0;
+            }
+        }
+        return cnt;
     }
+    public int solution(int[] stones, int k) {
+        int answer = 0;
 
-	public static void main(String[] args){
-		Solution T = new Solution();
-		System.out.println(Arrays.toString(T.solution(new int[]{2, 4, 5, 3, 2, 1, 4, 2, 5, 1})));
-	}
+        int lt = 0;
+        int rt = Integer.MAX_VALUE; // 돌다리 숫자가 매우 최대면 최대로 사람수가 건널 수 있기 때문에 MAX로 설정.
+        while(lt<=rt){
+            int mid = (lt+rt) / 2;
+            if(count(mid, k, stones) < k){
+                lt = mid + 1;
+                answer = mid;
+            }
+            else{
+                rt = mid - 1;
+            }
+        }
+        return answer;
+    }
 }
-
-
-/* 출력
-3
-*/
