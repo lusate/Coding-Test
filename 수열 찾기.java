1937d9caf77a0fd9aa73e39bb9141bdbe1e0d869
sang woo park <95400441+lusate@users.noreply.github.com>
2023-03-29 23:06:31 +0900

    Update 수열 찾기.java

--- 수열 찾기.java
+++ 수열 찾기.java
@@ -27,9 +27,10 @@ class Main{
 	}
 	
 	public static void main(String[] args){
-		System.out.println(Arrays.toString(solution(new int[]{1, 10, 2, 3, 5, 6})));
-		System.out.println(Arrays.toString(solution(new int[]{1, 1, 6, 2, 2, 7, 3, 14})));
-		System.out.println(Arrays.toString(solution(new int[]{14, 4, 2, 6, 3, 10, 10, 5, 5, 7, 7, 14})));
+		Solution T = new Solution();
+		System.out.println(Arrays.toString(T.solution(new int[]{1, 10, 2, 3, 5, 6})));
+		System.out.println(Arrays.toString(T.solution(new int[]{1, 1, 6, 2, 2, 7, 3, 14})));
+		System.out.println(Arrays.toString(T.solution(new int[]{14, 4, 2, 6, 3, 10, 10, 5, 5, 7, 7, 14})));
 	}
 }
 
