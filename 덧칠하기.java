7f23c04c94099ed6372be54f6df6941c0a7750bf
sang woo park <95400441+lusate@users.noreply.github.com>
2023-04-01 18:44:53 +0900

    Update 덧칠하기.java

--- 덧칠하기.java
+++ 덧칠하기.java
@@ -28,3 +28,32 @@ class Solution {
 1
 4
 */
+
+
+// -------------------------------------------------------------------------------------------
+
+
+import java.util.*;
+class Solution{
+    public int solution(int n, int m, int[] section) {
+        int answer = 0;
+        int max = 0;
+
+        for (int i = 0; i < section.length; i++) {
+            if (section[i] < max) {
+                continue;
+            }
+            answer += 1;
+            max = section[i] + m;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+        Solution T = new Solution();
+        System.out.println(T.solution(8, 4, new int[]{2, 3, 6}));
+        System.out.println(T.solution(5, 4, new int[]{1, 3}));
+        System.out.println(T.solution(4, 1, new int[]{1, 2, 3, 4}));
+    }
+}
