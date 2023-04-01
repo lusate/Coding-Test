6517ba2262f8aba7240ceb55f69cbc05f437fede
sang woo park <95400441+lusate@users.noreply.github.com>
2023-04-01 15:11:26 +0900

    Create 섬 연결하기.java

---
+++ 섬 연결하기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution{
+    public int solution(int n, int[][] costs) {
+
+    }
+
+    public static void main(String[] args){
+        Solution T = new Solution();
+        System.out.println(T.solution(4, new int[][]{{0,1,1}, {0,2,2}, {1,2,5}, {1,3,1}, {2,3,8}}));
+
+    }
+}
