2e989b36c455329658ed6eab2ee1ca6b78b39a60
sang woo park <95400441+lusate@users.noreply.github.com>
2023-04-09 15:20:58 +0900

    Update 인사고과.java

--- 인사고과.java
+++ 인사고과.java
@@ -19,28 +19,28 @@ class Solution {
         int wanhooSum = a + b;
 
         for(int[] x : scores){
-            int num1 = x[0];
-            int num2 = x[1];
-            int sum = num1 + num2;
+            int sum = x[0] + x[1];
 
             //완호가 인센티브를 받지 못하는 경우
-            if(a < num1 && b < num2) return -1;
+            if(a < x[0] && b < x[1]) return -1;
 
             if(sum > wanhooSum){
                 //완호보다 합이 큰 사원들 중에 인센티브를 받을 수 있다면 랭크를 +
-		//완호보다 점수 합이 작다면 랭크를 셀 필요가 없음.
+                //완호보다 점수 합이 작다면 랭크를 셀 필요가 없음.
                 if(Incentive(x, scores)) answer++;
+                // 이렇게 2차원 배열에서 하나를 선택해서 모두와 비교할 때는 파라미터로
+                // 1차원 배열짜리 하나 추가하여 풀어보기.
             }
         }
 
         return answer;
     }
-	
-	public static void main(String[] args){
-		Solution T = new Solution();
-		System.out.println(T.solution(new int[][]{{2, 2}, {1, 4}, {3, 2}, {3, 2}, {2, 1}}));
-    System.out.println(T.solution(new int[][]{{2, 1}, {2, 2}, {3, 2}, {4, 2}, {2, 1}}));
-	}
+
+    public static void main(String[] args){
+        Solution T = new Solution();
+        System.out.println(T.solution(new int[][]{{2, 2}, {1, 4}, {3, 2}, {3, 2}, {2, 1}}));
+        System.out.println(T.solution(new int[][]{{2, 1}, {2, 2}, {3, 2}, {4, 2}, {2, 1}}));
+    }
 }
 
 /* 출력
