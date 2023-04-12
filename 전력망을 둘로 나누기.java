452d97fac5917a6b1959ddfd85569018bcb0af2d
sang woo park <95400441+lusate@users.noreply.github.com>
2023-04-12 15:56:35 +0900

    Update 전력망을 둘로 나누기.java

--- 전력망을 둘로 나누기.java
+++ 전력망을 둘로 나누기.java
@@ -1,5 +1,59 @@
 import java.util.*;
 class Solution {
+    int answer;
+    int[][] map;
+    boolean[] visit;
+
+    public int dfs(int n, int cnt) {
+        visit[cnt] = true;
+        int child = 1;
+        for (int i = 1; i <= n; i++) {
+            if (map[cnt][i] == 1 && !visit[i]) {
+                visit[i] = true;
+                child += dfs(n, i);
+            }
+        }
+        answer = Math.min(answer, Math.abs(child - (n - child)));
+
+        return child;
+    }
+    public int solution(int n, int[][] wires) {
+        answer = n;
+        map = new int[n + 1][n + 1];
+        visit = new boolean[n + 1];
+
+        for (int[] x : wires) {
+            int a = x[0];
+            int b = x[1];
+
+            map[a][b] = 1;
+            map[b][a] = 1;
+        }
+        dfs(n, 1);
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+        Solution T = new Solution();
+        System.out.println(T.solution(9, new int[][]{{1, 3}, {2, 3}, {3, 4}, {4, 5}, {4, 6}, {4, 7}, {7, 8}, {7, 9}}));
+        System.out.println(T.solution(4, new int[][]{{1, 2}, {2, 3}, {3, 4}}));
+        System.out.println(T.solution(7, new int[][]{{1, 2}, {2, 7}, {3, 7}, {3, 4}, {4, 5}, {6, 7}}));
+    }
+}
+
+
+/* 출력
+[2, 3]
+[6, 6]
+[0, 2]
+*/
+
+
+// -------------------------------------------------------------------------------------------
+
+import java.util.*;
+class Solution {
     int[][] map;
 
     public int bfs(int n, int st) {
@@ -58,9 +112,3 @@ class Solution {
         System.out.println(T.solution(7, new int[][]{{1, 2}, {2, 7}, {3, 7}, {3, 4}, {4, 5}, {6, 7}}));
     }
 }
-
-/* 출력
-[2, 3]
-[6, 6]
-[0, 2]
-*/
