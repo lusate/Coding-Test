f7efbf7e7b61f801db66cca209a12c1fbd8ecdbc
sang woo park <95400441+lusate@users.noreply.github.com>
2023-04-28 17:19:44 +0900

    Update 이진수 정렬.java

--- 이진수 정렬.java
+++ 이진수 정렬.java
@@ -1,6 +1,6 @@
 import java.util.*;
-class Main{
-	private static int[] solution(int[] nums){
+class Solution{
+	public int[] solution(int[] nums){
 		int[] answer = new int[nums.length];
 		int[][] res = new int[nums.length][2];
 
@@ -29,9 +29,10 @@ class Main{
 	}
 	
 	public static void main(String[] args){
-		System.out.println(Arrays.toString(solution(new int[]{5, 6, 7, 8, 9})));
-		System.out.println(Arrays.toString(solution(new int[]{5,4,3,2,1})));
-		System.out.println(Arrays.toString(solution(new int[]{12, 5, 7, 23, 45, 21, 17})));
+		Solution T = new Solution();
+        	System.out.println(Arrays.toString(T.solution(new int[]{5, 6, 7, 8, 9})));
+        	System.out.println(Arrays.toString(T.solution(new int[]{5,4,3,2,1})));
+        	System.out.println(Arrays.toString(T.solution(new int[]{12, 5, 7, 23, 45, 21, 17})));
 	}
 }
 
