ff042f8cf261ba980d831e508e28e21884f398d8
sang woo park <95400441+lusate@users.noreply.github.com>
2023-04-28 17:38:57 +0900

    Update 최소 회의실 개수.java

--- 최소 회의실 개수.java
+++ 최소 회의실 개수.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class Main{
+class Solution{
 	private static int solution(int[][] meetings){
 		int answer = 0;
 		ArrayList<int[]> arr = new ArrayList<>();
@@ -25,9 +25,10 @@ class Main{
 	}
 	
 	public static void main(String[] args){
-		System.out.println(solution(new int[][]{{0, 10}, {12, 25}, {5, 15}}));
-		System.out.println(solution(new int[][]{{1, 30}, {2, 15}, {3, 10}, {4, 12}, {6, 10}}));
-		System.out.println(solution(new int[][]{{3, 9}, {1, 10}, {5, 8}, {10, 15}, {9, 14}, {12, 14}, {15, 20}}));
+		Solution T = new Solution();
+		System.out.println(T.solution(new int[][]{{0, 10}, {12, 25}, {5, 15}}));
+		System.out.println(T.solution(new int[][]{{1, 30}, {2, 15}, {3, 10}, {4, 12}, {6, 10}}));
+		System.out.println(T.solution(new int[][]{{3, 9}, {1, 10}, {5, 8}, {10, 15}, {9, 14}, {12, 14}, {15, 20}}));
 	}
 }
 
