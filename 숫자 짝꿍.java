a1e010aab842c7fb912379d8008bd4640be610b7
sang woo park <95400441+lusate@users.noreply.github.com>
2023-05-01 16:58:11 +0900

    Update 숫자 짝꿍.java

--- 숫자 짝꿍.java
+++ 숫자 짝꿍.java
@@ -5,7 +5,7 @@ class Solution {
         int[] mapY = new int[10];
 
         for (char c : X.toCharArray()) {
-            mapX[c - 48]++; // 인덱스의 숫자가 X에 몇 개있는지를 저장
+            mapX[c - 48]++;// 인덱스의 숫자가 X에 몇 개있는지를 저장
             // ex) 100이면 mapX[0]은 0이 100에 2개 있기 때문에 mapX[0] = 2
             // mapX[1] 은 1이 100에 1개 있기 때문에 mapX[1] = 1
         }
@@ -15,20 +15,23 @@ class Solution {
 
         StringBuilder answer = new StringBuilder();
         for (int i = 9; i >= 0; i--) {
-            for (int j=0; j<Math.min(mapX[i], mapY[i]); j++) {
-                answer.append(i); // mapX와 mapY의 인덱스로 answer를 구한다.
+            while (mapX[i] > 0 && mapY[i] > 0) { // 같은 숫자가 있다는 뜻
+                answer.append(i);
+
+                mapX[i]--;
+                mapY[i]--;
+                // ex) mapX에는 3이라는 숫자가 2개 있고 mapY에는 3이라는 숫자가 3개있을 경우
+                // 2개로 해야 함.
             }
         }
 
-
         if ("".equals(answer.toString())) {
             return "-1";
-        } else if (answer.toString().charAt(0) == 48) {
-            return "0";
-        } else{
-            return answer.toString();
         }
-
+        else if (answer.toString().charAt(0) == 48) {
+            return "0";
+        }
+        else return answer.toString();
     }
 
     public static void main(String[] args){
@@ -38,13 +41,16 @@ class Solution {
         System.out.println(T.solution("100", "123450"));
         System.out.println(T.solution("12321", "42531"));
         System.out.println(T.solution("5525", "1255"));
+        System.out.println(T.solution("3403", "13203"));
     }
 }
 
 
 /* 출력
-"-1"
-"0"
-"10"
-"321"
+-1
+0
+10
+321
+552
+330
 */
