411bbb1744ab398c3d6cc2728257244bf7d6f431
sang woo park <95400441+lusate@users.noreply.github.com>
2023-05-11 16:38:23 +0900

    백준

--- 운동.java
+++ 운동.java
@@ -2,8 +2,8 @@ import java.util.*;
 import java.io.*;
 public class Main {
     static int INF = 10000000;
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int v = Integer.parseInt(st.nextToken());
@@ -12,6 +12,9 @@ public class Main {
         int dy[][] = new int[v+1][v+1];
         for(int i=1;i<=v;i++){
             for(int j=1;j<=v;j++){
+                if(i == j){
+                    dy[i][j] = 0;
+                }
                 dy[i][j] = INF; //v번 도시에서 v번 도시로 가는 거리 모두 INF로 초기화
             }
         }
@@ -21,7 +24,7 @@ public class Main {
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
-            dy[a][b] = c; //일반통행도로 
+            dy[a][b] = c; //일반통행도로
         }
 
 
@@ -29,7 +32,9 @@ public class Main {
         for(int k=1;k<=v;k++){
             for(int i=1;i<=v;i++){
                 for(int j=1;j<=v;j++){
-                    dy[i][j] = Math.min(dy[i][j], dy[i][k] + dy[k][j]);
+                    if (dy[i][j] > dy[i][k] + dy[k][j]) {
+                        dy[i][j] = dy[i][k] + dy[k][j];
+                    }
                 }
             }
         }
@@ -43,16 +48,18 @@ public class Main {
             }
         }
 
-        System.out.println(min == Integer.MAX_VALUE?-1:min);
-	}
+        System.out.println(min == Integer.MAX_VALUE ? -1 : min);
+    }
 }
 
-//플로이드 와샬
-
+/* 입력
 3 4
 1 2 1
 3 2 1
 1 3 5
 2 3 2
-  
-//답 3  
+*/
+
+/* 출력
+3
+*/
