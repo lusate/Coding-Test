45106f6cfb39afecedf887f77cc220a080b59c19
sang woo park <95400441+lusate@users.noreply.github.com>
2024-01-16 23:41:25 +0900

    Solved

--- 이진트리 순회(DFS).java
+++ 이진트리 순회(DFS).java
@@ -24,3 +24,56 @@ class Main{
 		T.DFS(1);
 	}
 }
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+package 인프런_입문.BFSDFS그래프;
+
+class Node {
+    int data; // 1 ~ 7
+    Node lt, rt; // 왼쪽과 오른쪽 자식 노드
+
+    Node(int val) {
+        data = val;
+        lt = rt = null;
+    }
+}
+public class 이진트리순회 {
+    Node root;
+    public void DFS(Node root) {
+        if (root == null) { // 말단 노드까지 왔으므로 null
+            return;
+        } else {
+            // 전위 순회
+            System.out.print(root.data + " ");
+            DFS(root.lt);
+            DFS(root.rt);
+
+
+            // 중위순회
+            DFS(root.lt);
+            System.out.print(root.data + " ");
+            DFS(root.rt);
+
+
+            // 후위순회
+            DFS(root.lt);
+            DFS(root.rt);
+            System.out.print(root.data + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        이진트리순회 tree = new 이진트리순회();
+        tree.root = new Node(1);
+        tree.root.lt = new Node(2);
+        tree.root.rt = new Node(3);
+        tree.root.lt.lt = new Node(4);
+        tree.root.lt.rt = new Node(5);
+        tree.root.rt.lt = new Node(6);
+        tree.root.rt.rt = new Node(7);
+        tree.DFS(tree.root);
+    }
+}
+
