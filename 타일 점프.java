bcaaec9c37f8c5f8d2058e5394777f2646e6fe6b
sang woo park <95400441+lusate@users.noreply.github.com>
2024-05-05 23:23:08 +0900

    다시 복습

--- 타일 점프.java
+++ 타일 점프.java
@@ -8,7 +8,7 @@ class Main { //시작점에서 도착점으로 이동을 하므로 BFS로 한다
 		ch[0] = 1; //0번 인덱스에서 출발하므로 처음 ch[0]은 1로 초기화
 		int L=0;
 		while(!Q.isEmpty()){
-			int len=Q.size();
+			int len=Q.size(); // 사용할 점프만을 선택하기 때문에 q에 들어가있는 점프만 사용함.
 			for(int i=0; i<len; i++){
 				int x = Q.poll();
 				// nums -> 2,2,0,2,1,1, x는 인덱스로 nums 안에 값 이하로 점프함
@@ -25,7 +25,7 @@ class Main { //시작점에서 도착점으로 이동을 하므로 BFS로 한다
 		}
 		return -1;
 	}
-		
+
 	public static void main(String[] args){
 		Main T = new Main();
 		System.out.println(T.solution(new int[]{2, 2, 0, 2, 1, 1}));
