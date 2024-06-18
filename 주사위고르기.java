21d62f01c6826a0f3dbd65944d939e95a81fd63f
sang woo park <95400441+lusate@users.noreply.github.com>
2024-06-18 17:51:52 +0900

    UnSolved
    1. DFS를 사용하려는 건 맞았지만 winning을 구할 때 이분 탐색을 통해서 구해야 할 줄은 몰랐다. (아직까지 이분탐색 count하는 부분 이해가 가지 않음.)
    
    2. arrA와 arrB에 각각의 주사위 2개를 합한 값이 들어가야 한다. 그러면 총 36개의 값이 들어가게 됨.
    
    3. 주사위 2개를 선택했을 때 A가 몇 번씩 이기는지 구했으면 이긴 최댓값을 가진 choice(A가 선택한 주사위 n/2개)를 answer에 넣어주면 된다.

--- 주사위고르기.java
+++ 주사위고르기.java
@@ -1,35 +1,123 @@
-package 프로그래머스;
-
 import java.util.*;
 
-public class 주사위고르기 {
+public class Solution {
     static int n;
+    static List<Integer> choice = new ArrayList<>();
+    static int[][] dices;
     static ArrayList<Integer> arrA;
     static ArrayList<Integer> arrB;
+    static int[] answer;
+    static int max = Integer.MIN_VALUE;
+
+    /**
+     * 합을 구하기 위한 메서드
+     * arr은 주사위 굴려서 나온 2개의 숫자를 더한 값이다.
+     */
+    public static void makeArr(int L, int[][] dice, int sum, List<Integer> arr) {
+        if (L == n / 2) {
+            arr.add(sum);
+            return;
+        }
+        for (int i = 0; i < 6; i++) {
+            int newSum = sum + dice[L][i];
+            makeArr(L + 1, dice, newSum, arr);
+        }
+    }
+
+    /**
+     * arrA, arrB 각각에 A가 선택한 주사위의 값을 넣기 위해서 choice.contains
+     *
+     */
+    public static void makeArrAB() {
+        arrA = new ArrayList<>();
+        arrB = new ArrayList<>();
+
+        int[][] diceA = new int[n / 2][6];
+        int[][] diceB = new int[n / 2][6];
+        int a = 0, b = 0;
+        for (int i = 0; i < n; i++) {
+            if (choice.contains(i)) { // choice는 A가 선택한 주사위
+                diceA[a] = dices[i];
+                a++;
+            }
+            else {
+                diceB[b] = dices[i];
+                b++;
+            }
+        }
+
+        /**
+         * makeArr을 통해 arrA와 arrB에 주사위에서 나온 숫자 2개를 더한 값이 들어간다.
+         * 그러면 arrA와 arrB에 각각 36개의 sum 값이 들어간다.
+         */
+        makeArr(0, diceA, 0, arrA);
+        makeArr(0, diceB, 0, arrB);
+    }
+
+    /**
+     * 이분 탐색을 통해 A의 합이 더 큰 경우를 찾아서 count
+     */
+    public static int calculateWinningPercentage() {
+        int count = 0;
+        makeArrAB();
+
+        Collections.sort(arrB);
+
+        for (int i = 0; i < arrA.size(); i++) {
+            int number = arrA.get(i); // number는 A 주사위의 합
+            int lt = 0, rt = arrB.size() - 1;
+            int idx = Integer.MIN_VALUE;
+            while (lt <= rt) {
+                int mid = (lt + rt) / 2;
+                if (arrB.get(mid) < number) { // number가 더 크므로 A가 이기는 경우
+                    lt = mid + 1;
+                    idx = Math.max(idx, mid);
+                }
+                else{
+                    rt = mid - 1;
+                }
+            }
+
+            if (idx != Integer.MIN_VALUE) {
+                count += idx + 1;
+            }
+        }
+        return count;
+    }
     public void dfs(int L, int start) { // A가 이길 수 있는 경우의 수
         if (L == n / 2) { // A가 n/2개 선택했을 때
+            int winning = calculateWinningPercentage();
+            System.out.println(choice.get(0) + ", " + choice.get(1) + " => " + winning);
 
+            if (max < winning) {
+                max = winning;
+                for (int i = 0; i < choice.size(); i++) {
+                    answer[i] = choice.get(i) + 1; // 주사위 번호는 1부터이므로 1 더해줌.
+                }
+            }
+            return;
         }
 
         for (int i = start; i < n; i++) {
+            choice.add(i);
             dfs(L + 1, i + 1);
+            choice.remove(choice.size() - 1);
         }
     }
     public int[] solution(int[][] dice) {
         n = dice.length;
-        int[] answer = new int[n / 2];
-
-
+        dices = dice;
+        answer = new int[n / 2];
         dfs(0, 0);
 
         return answer;
     }
 
     public static void main(String[] args) {
-        주사위고르기 T = new 주사위고르기();
-        System.out.println(T.solution(new int[][]{{1,2,3,4,5,6}, {3,3,3,3,4,4}, {1,3,3,4,4,4}, {1,1,4,4,5,5}}));
-        System.out.println(T.solution(new int[][]{{1,2,3,4,5,6}, {2,2,4,4,6,6}}));
-        System.out.println(T.solution(new int[][]{{40, 41, 42, 43, 44, 45}, {43, 43, 42, 42, 41, 41}, {1, 1, 80, 80, 80, 80}, {70, 70, 1, 1, 70, 70}}));
+        Solution T = new Solution();
+        System.out.println(Arrays.toString(T.solution(new int[][]{{1, 2, 3, 4, 5, 6}, {3, 3, 3, 3, 4, 4}, {1, 3, 3, 4, 4, 4}, {1, 1, 4, 4, 5, 5}})));
+//        System.out.println(Arrays.toString(T.solution(new int[][]{{1, 2, 3, 4, 5, 6}, {2, 2, 4, 4, 6, 6}})));
+//        System.out.println(Arrays.toString(T.solution(new int[][]{{40, 41, 42, 43, 44, 45}, {43, 43, 42, 42, 41, 41}, {1, 1, 80, 80, 80, 80}, {70, 70, 1, 1, 70, 70}})));
     }
 }
 
