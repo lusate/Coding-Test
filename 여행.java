8daaf909518d92cc56ea28e8450f269293307a46
sang woo park <95400441+lusate@users.noreply.github.com>
2024-07-08 14:22:56 +0900

    DP

--- 여행.java
+++ 여행.java
@@ -10,6 +10,7 @@ class Node{
         this.score = score;
     }
 }
+
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
